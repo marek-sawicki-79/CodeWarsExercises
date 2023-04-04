adbe2de4dfd00a6746a0cc67d5799e24e9d9ea2b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-04 14:57:23 +0200

    1 test not pass

--- CheckSameCases/Kata/Kata.cs
+++ CheckSameCases/Kata/Kata.cs
@@ -15,7 +15,22 @@ namespace Kata
             var bToLower = Regex.IsMatch(b.ToString(), @"^[a-z]+$");
             var aToUpper = Regex.IsMatch(a.ToString(), @"^[A-Z]+$");
             var bToUpper = Regex.IsMatch(b.ToString(), @"^[A-Z]+$");
-            //if (Regex.IsMatch(a.ToString(), @"^[a-z]+$"))
+            //var aIsLetter = Regex.IsMatch(a.ToString(), @"^[a-zA-Z]+$");
+            //var bIsLetter = Regex.IsMatch(b.ToString(), @"^[a-zA-Z]+$");
+            var aIsLetter = Char.IsLetter(a);
+            var bIsLetter = Char.IsLetter(b);
+            if ((aToLower == true && bToLower == true) || (aToUpper == true && bToUpper == true))
+            {
+                return 1;
+            }
+            else if ((aToLower == true || bToUpper == true) || (aToUpper == true && bToLower == true))
+            {
+                return 0;
+            }
+            else if (!bIsLetter || !bIsLetter)
+            {
+                return -1;
+            }
             return 0;
         }
     }
