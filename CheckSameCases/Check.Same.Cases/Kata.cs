5c27e4a868408164ad67385ae14e977392fca017
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-04 14:39:55 +0200

    SameCase exercise prep

---
+++ CheckSameCases/Check.Same.Cases/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameCase
+{
+    public class Kata
+    {
+        public static int SameCase(char a, char b)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CheckSameCases/Kata/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Kata
+    {
+        public static int SameCase(char a, char b)
+        {
+            var aToLower = Regex.IsMatch(a.ToString(), @"^[a-z]+$");
+            var bToLower = Regex.IsMatch(b.ToString(), @"^[a-z]+$");
+            var aToUpper = Regex.IsMatch(a.ToString(), @"^[A-Z]+$");
+            var bToUpper = Regex.IsMatch(b.ToString(), @"^[A-Z]+$");
+            //if (Regex.IsMatch(a.ToString(), @"^[a-z]+$"))
+            return 0;
+        }
+    }
+}
