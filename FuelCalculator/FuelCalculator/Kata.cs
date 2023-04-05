403eaf5ef97dcc4252eeedb890c1f30c0ee31112
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-05 23:25:30 +0200

    1st solution

--- FuelCalculator/FuelCalculator/Kata.cs
+++ FuelCalculator/FuelCalculator/Kata.cs
@@ -10,7 +10,33 @@ namespace FuelCalculator
     {
         public static double FuelPrice(double litres, double pricePerLitre)
         {
-            return 0;
+            double fuelPrice = 0;
+            if (litres > 0 && litres < 2)
+            {
+                fuelPrice = litres * pricePerLitre;
+            }
+            else if (litres >= 2 && litres < 4)
+            {
+                fuelPrice = litres * (pricePerLitre - 0.05);
+            }
+            else if (litres >= 4 && litres < 6)
+            {
+                fuelPrice = litres * (pricePerLitre - 0.1);
+            }
+            else if (litres >= 6 && litres < 8)
+            {
+                fuelPrice = litres * (pricePerLitre - 0.15);
+            }
+            else if (litres >= 8 && litres < 10)
+            {
+                fuelPrice = litres * (pricePerLitre - 0.2);
+            }
+            else if (litres >= 10)
+            {
+                fuelPrice = litres * (pricePerLitre - 0.25);
+            }
+            double result = Math.Round(fuelPrice,2);
+            return result;
         }
     }
 }
