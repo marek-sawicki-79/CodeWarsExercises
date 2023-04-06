e1192191f2b2f268e4c734fe1ed057fe183b354b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-06 15:41:12 +0200

    bool to word setup

---
+++ BoolToString/BoolToString/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolToString
+{
+    internal class Kata
+    {
+        public static string boolToWord(bool word)
+        {
+            i
+        }
+    }
+}
---
+++ BoolToString/BoolToString/UnitTest1.cs
@@ -0,0 +1,30 @@
+namespace BoolToString
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class boolToWordTest
+    {
+
+        [Test]
+        public void boolToWordReturned1()
+        {
+            Assert.AreEqual("Yes", Kata.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+
+        [Test]
+        public void boolToWordReturned2()
+        {
+            Assert.AreEqual("No", Kata.boolToWord(false));
+            Console.WriteLine("Expected No");
+        }
+
+        [Test]
+        public void boolToWordReturned3()
+        {
+            Assert.AreEqual("Yes", Kata.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+    }
