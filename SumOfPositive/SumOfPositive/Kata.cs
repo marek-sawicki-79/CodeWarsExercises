84a47571489030b9d440566a9b95a65542e0554b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-09 19:31:37 +0200

    sum of positive exercise prep

---
+++ SumOfPositive/SumOfPositive/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfPositive
+{
+    public class Kata
+    {
+        public static int PositiveSum(int[] arr)
+        {
+            // Your code here
+            return 0;
+        }
+    }
+}
---
+++ SumOfPositive/SumOfPositive/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace SumOfPositive
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 15)]
+        [TestCase(new int[] { 1, -2, 3, 4, 5 }, ExpectedResult = 13)]
+        [TestCase(new int[] { -1, 2, 3, 4, -5 }, ExpectedResult = 9)]
+        [TestCase(new int[] { }, ExpectedResult = 0)]
+        [TestCase(new int[] { -1, -2, -3, -4, -5 }, ExpectedResult = 0)]
+        public static int ExampleTest(int[] arr)
+        {
+            return Kata.PositiveSum(arr);
+        }
+    }
+}
