47c550473afa3c642d30d76d70a3a5f6f2b3ec67
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-10 20:37:06 +0200

    1st

---
+++ CountOfPositivesSumOfNegatives/CountOfPositivesSumOfNegatives/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CountOfPositivesSumOfNegatives
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
