860e2ddd7f0d4fa9fe5930a52c3ebb1d33964cf0
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-10 20:46:23 +0200

    exercise prep

---
+++ CountOfPositivesSumOfNegatives/CountOfPositivesSumOfNegatives/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfPositivesSumOfNegatives
+{
+    internal class Kata
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            
+            return null; 
+        }
+    }
+}
