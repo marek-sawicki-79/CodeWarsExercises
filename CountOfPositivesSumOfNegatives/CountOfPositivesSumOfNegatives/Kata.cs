179d8b81f16ba61d30acff7400a215875b72589f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-10 21:02:04 +0200

    not there yet

--- CountOfPositivesSumOfNegatives/CountOfPositivesSumOfNegatives/Kata.cs
+++ CountOfPositivesSumOfNegatives/CountOfPositivesSumOfNegatives/Kata.cs
@@ -12,8 +12,12 @@ namespace CountOfPositivesSumOfNegatives
         {
             int positvieCount = input.Count(b => b > 0);
             int negativeSum = input.Where(b => b < 0).Sum();
-            int[] result = new int[] { positvieCount, negativeSum };
-            return result; 
+            int[] result = new int[0];
+            if (input != null || input.Length != 0)
+            {
+                result = new int[] { positvieCount, negativeSum };
+            }
+            return result;
         }
     }
 }
