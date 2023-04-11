95f3e2ca9d312a95cf8d3c9667fe16efaae329a2
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-11 18:31:16 +0200

    keep hydrated exercise prep

---
+++ KeepHydrated/KeepHydrated/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepHydrated
+{
+    public class Kata
+    {
+        public static int Litres(double time)
+        {
+            //The fun begins here.
+            return 0;
+        }
+    }
+}
--- KeepHydrated/KeepHydrated/UnitTest1.cs
+++ KeepHydrated/KeepHydrated/UnitTest1.cs
@@ -1,16 +1,51 @@
 namespace KeepHydrated
 {
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public static void Test1()
         {
+            Assert.AreEqual(1, Kata.Litres(2));
         }
 
         [Test]
-        public void Test1()
+        public static void Test2()
         {
-            Assert.Pass();
+            Assert.AreEqual(0, Kata.Litres(1.4));
         }
+
+        [Test]
+        public static void Test3()
+        {
+            Assert.AreEqual(6, Kata.Litres(12.3));
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            Assert.AreEqual(0, Kata.Litres(0.82));
+        }
+
+        [Test]
+        public static void Test5()
+        {
+            Assert.AreEqual(5, Kata.Litres(11.8));
+        }
+
+        [Test]
+        public static void Test6()
+        {
+            Assert.AreEqual(893, Kata.Litres(1787));
+        }
+        [Test]
+        public static void Test7()
+        {
+            Assert.AreEqual(0, Kata.Litres(0));
+        }
+
     }
 }
