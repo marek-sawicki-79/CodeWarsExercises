17ecead14f3bfeef9725a437ee4d4060fe022334
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-12 11:38:37 +0200

    1st attempt

---
+++ EvenOrOdd/EvenOrOdd/SolutionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    public class SolutionClass
+    {
+        public static string EvenOrOdd(int number)
+        {
+            int modulo = number % 2;
+            string result = (modulo != 0) ? "Odd" : "Even";
+            return result;
+        }
+    }
+}
--- EvenOrOdd/EvenOrOdd/UnitTest1.cs
+++ EvenOrOdd/EvenOrOdd/UnitTest1.cs
@@ -1,16 +1,21 @@
 namespace EvenOrOdd
 {
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
+    using System;
+    using NUnit.Framework;
+
+
+        [TestFixture]
+        public class SolutionTest
         {
+            [Test]
+            public void MyTest()
+            {
+                Assert.AreEqual("Even", SolutionClass.EvenOrOdd(2));
+                Assert.AreEqual("Odd", SolutionClass.EvenOrOdd(1));
+                Assert.AreEqual("Even", SolutionClass.EvenOrOdd(0));
+                Assert.AreEqual("Odd", SolutionClass.EvenOrOdd(7));
+                Assert.AreEqual("Odd", SolutionClass.EvenOrOdd(-1));
+            }
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
 }
