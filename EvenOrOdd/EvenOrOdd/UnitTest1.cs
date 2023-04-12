4d5f79760adaab5afe8d821a24370148204f4113
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-12 11:29:47 +0200

    1st

---
+++ EvenOrOdd/EvenOrOdd/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EvenOrOdd
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
