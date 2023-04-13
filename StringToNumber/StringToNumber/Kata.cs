9ba025435a79975c6317d6e54af8e66451aa16ca
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-13 21:27:47 +0200

    exercise prep

---
+++ StringToNumber/StringToNumber/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToNumber
+{
+    public class Kata
+    {
+        public static int StringToNumber(String str)
+        {
+            //TODO: Convert str into a number
+        }
+    }
+}
