2ba71d02e43400ded7b236bba484fc04e49c79c9
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-13 21:20:52 +0200

    exercise prep

---
+++ NumberToString/NumberToString/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace NumberToString
+{
+    [TestFixture]
+    public class KataTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(67).Returns("67");
+                yield return new TestCaseData(79585).Returns("79585");
+                yield return new TestCaseData(1 + 2).Returns("3");
+                yield return new TestCaseData(1 - 2).Returns("-1");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(int num) => Kata.NumberToString(num);
+    }
+}
