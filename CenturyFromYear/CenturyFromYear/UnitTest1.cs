89516bdbbfa1820d1e7f262d47dcc90043115613
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-14 16:12:05 +0200

    exercise prep

---
+++ CenturyFromYear/CenturyFromYear/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace CenturyFromYear
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(18, Kata.СenturyFromYear(1705));
+            Assert.AreEqual(19, Kata.СenturyFromYear(1900));
+            Assert.AreEqual(17, Kata.СenturyFromYear(1601));
+            Assert.AreEqual(20, Kata.СenturyFromYear(2000));
+        }
+    }
+}
