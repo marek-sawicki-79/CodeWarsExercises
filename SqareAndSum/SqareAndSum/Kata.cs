c48a4c971ed1216a358ea987089258c4bebcd765
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-14 18:38:54 +0200

    done

--- SqareAndSum/SqareAndSum/Kata.cs
+++ SqareAndSum/SqareAndSum/Kata.cs
@@ -10,7 +10,12 @@ namespace SqareAndSum
     {
         public static int SquareSum(int[] numbers)
         {
-
+            int sqrtSum = 0;
+            foreach(int number in numbers)
+            {
+                sqrtSum = sqrtSum + number * number;
+            }
+            return sqrtSum;
         }
     }
 }
