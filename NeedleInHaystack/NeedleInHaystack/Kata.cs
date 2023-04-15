fcf63bb8ef931e9990acfffd5b277319bd894224
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-15 19:53:09 +0200

    exercise prep

---
+++ NeedleInHaystack/NeedleInHaystack/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedleInHaystack
+{
+    public class Kata
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            //Code goes here!
+        }
+    }
+}
