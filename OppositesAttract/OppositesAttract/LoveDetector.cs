e0eda9170993e2d4989fd89c3e59b13298c4114e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-15 20:05:58 +0200

    exercise prep

---
+++ OppositesAttract/OppositesAttract/LoveDetector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppositesAttract
+{
+    public class LoveDetector
+    {
+        public static bool lovefunc(int flower1, int flower2)
+        {
+            //Moment of truth...
+        }
+    }
+}
---
+++ OppositesAttract/OppositesAttract/UnitTest1.cs
@@ -0,0 +1,30 @@
+namespace OppositesAttract
+{
+    [TestFixture]
+    public class LoveTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, LoveDetector.lovefunc(1, 4));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(false, LoveDetector.lovefunc(2, 2));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(true, LoveDetector.lovefunc(0, 1));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(false, LoveDetector.lovefunc(0, 0));
+        }
+    }
+}
