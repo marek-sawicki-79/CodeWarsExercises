f3d1b23aa05468ccc46aecb480aae919ab1eb04f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-16 21:55:28 +0200

    stupid logic error

--- CalculateBMI/CalculateBMI/Kata.cs
+++ CalculateBMI/CalculateBMI/Kata.cs
@@ -10,7 +10,11 @@ namespace CalculateBMI
     {
         public static string Bmi(double weight, double height)
         {
-            
+            double bmi = height / (weight * weight);
+            if (bmi <= 18.5) return "Underweight";
+            else if (bmi > 18.5 && bmi <= 25.0) return "Normal";
+            else if (bmi > 25.0 && bmi <= 30.0) return "Overweight";
+            else return "Obese";
         }
     }
 }
