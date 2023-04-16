1ef8becd86d49bdfd1054fbd22596fd88a7e789e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-16 21:28:33 +0200

    exercise prep

---
+++ HowGoodAreYouReally/HowGoodAreYouReally/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowGoodAreYouReally
+{
+    public class Kata
+    {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            //Insert brain here
+        }
+    }
+}
