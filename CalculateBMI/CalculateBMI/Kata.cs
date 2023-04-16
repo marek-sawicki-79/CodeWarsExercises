1ca711af8d6f567b13b309911d7dddabae914a6b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-16 22:00:29 +0200

    Very curious CodeWars Solution

--- CalculateBMI/CalculateBMI/Kata.cs
+++ CalculateBMI/CalculateBMI/Kata.cs
@@ -8,13 +8,15 @@ namespace CalculateBMI
 {
     public class Kata
     {
-        public static string Bmi(double weight, double height)
-        {
-            double bmi = weight / (height * height);
-            if (bmi <= 18.5) return "Underweight";
-            else if (bmi > 18.5 && bmi <= 25.0) return "Normal";
-            else if (bmi > 25.0 && bmi <= 30.0) return "Overweight";
-            else return "Obese";
-        }
+        public static string Bmi(double weight, double height) => (weight = weight / height / height) > 30 ? "Obese" : weight > 25 ? "Overweight" : weight > 18.5 ? "Normal" : "Underweight";
+        //{
+        //    //double bmi = weight / (height * height);
+        //    //if (bmi <= 18.5) return "Underweight";
+        //    //else if (bmi > 18.5 && bmi <= 25.0) return "Normal";
+        //    //else if (bmi > 25.0 && bmi <= 30.0) return "Overweight";
+        //    //else return "Obese";
+
+
+        //}
     }
 }
