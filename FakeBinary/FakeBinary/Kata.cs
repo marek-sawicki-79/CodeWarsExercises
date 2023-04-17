b871c37cef6c558ac4f9d98dccea6e1d65c3a8f9
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-17 12:32:44 +0200

    exercise prep

---
+++ FakeBinary/FakeBinary/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeBinary
+{
+    public class Kata
+    {
+        public static string FakeBin(string x)
+        {
+            return "";
+        }
+    }
+}
---
+++ FakeBinary/FakeBinary/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace FakeBinary
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("01011110001100111", Kata.FakeBin("45385593107843568"));
+            Assert.AreEqual("101000111101101", Kata.FakeBin("509321967506747"));
+            Assert.AreEqual("011011110000101010000011011", Kata.FakeBin("366058562030849490134388085"));
+        }
+    }
+}
