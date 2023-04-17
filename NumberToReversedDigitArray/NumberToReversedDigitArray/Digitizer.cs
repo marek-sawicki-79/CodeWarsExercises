5462fa9a5985e72664204fc1dadabf2b39bbd4a5
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-17 15:29:24 +0200

    very interesting and concise solution from CodeWars

--- NumberToReversedDigitArray/NumberToReversedDigitArray/Digitizer.cs
+++ NumberToReversedDigitArray/NumberToReversedDigitArray/Digitizer.cs
@@ -10,15 +10,16 @@ namespace NumberToReversedDigitArray
     {
         public static long[] Digitize(long n)
         {
-            string digitString = n.ToString();
-            long[] digits = new long [digitString.Length];
-            for (int i = digitString.Length - 1; i >= 0; i--)
-            {
-                digits[i] = long.Parse(digitString.Substring(i, 1));
-            }
+            //string digitString = n.ToString();
+            //long[] digits = new long [digitString.Length];
+            //for (int i = digitString.Length - 1; i >= 0; i--)
+            //{
+            //    digits[i] = long.Parse(digitString.Substring(i, 1));
+            //}
 
-            Array.Reverse(digits, 0, digits.Length);
-            return digits;
+            //Array.Reverse(digits, 0, digits.Length);
+            //return digits;
+            return n.ToString().Reverse().Select(t=>Convert.ToInt64(t.ToString())).ToArray();
         }
     }
 }
