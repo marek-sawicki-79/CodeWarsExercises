341c3c087171cd810eeb3df6ccfacfd801803150
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-17 22:47:03 +0200

    exercise prep

---
+++ IsHeGonnaSurvive/IsHeGonnaSurvive/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsHeGonnaSurvive
+{
+    public class Kata
+    {
+        public static bool Hero(int bullets, int dragons)
+        {
+            //Do Some Magic...
+        }
+    }
+}
