58bdeabfdabd4ef6b34d9dea5c5602d9429f6576
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-17 15:39:06 +0200

    1st attempt -> works

---
+++ SimpleMultiplication/SimpleMultiplication/Multiplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMultiplication
+{
+    public class Multiplier
+    {
+        public static int Multiply(int x)
+        {
+            var modulo = x % 2;
+            if (modulo == 0) return x * 8;
+            else return x * 9;
+        }
+    }
+}
