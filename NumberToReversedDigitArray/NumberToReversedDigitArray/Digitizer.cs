20ea6aa61475e111ef168ddd9c52ff935a86edbc
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-17 13:20:57 +0200

    exercise prep

---
+++ NumberToReversedDigitArray/NumberToReversedDigitArray/Digitizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToReversedDigitArray
+{
+    public class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            // Code goes here
+        }
+    }
+}
---
+++ NumberToReversedDigitArray/NumberToReversedDigitArray/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace NumberToReversedDigitArray
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(new long[] { 1, 3, 2, 5, 3 }, Digitizer.Digitize(35231));
+            Assert.AreEqual(new long[] { 0 }, Digitizer.Digitize(0));
+        }
+    }
+}
