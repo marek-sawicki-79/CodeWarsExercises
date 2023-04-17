32ef9e55c73d266b397d69e97d997f9e80e5ef9f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-17 13:33:10 +0200

    to messesd up, need to simplify

--- NumberToReversedDigitArray/NumberToReversedDigitArray/Digitizer.cs
+++ NumberToReversedDigitArray/NumberToReversedDigitArray/Digitizer.cs
@@ -10,7 +10,16 @@ namespace NumberToReversedDigitArray
     {
         public static long[] Digitize(long n)
         {
-            // Code goes here
+            string digitString = n.ToString();
+            long[] digits = new long [digitString.Length];
+            for (int i = digitString.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < digits.Length; j++)
+                {
+                    digits[j] = long.Parse(digitString.Substring(i, 1));
+                }
+            }
+            return digits;
         }
     }
 }
