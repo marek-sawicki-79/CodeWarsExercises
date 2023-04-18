147a519200da6997d07d63521bb1ec93f0e7ab66
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-18 10:38:49 +0200

    done

---
+++ StringToArray/StringToArray/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace StringToArray
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void _1_Example()
+        {
+            Assert.AreEqual(new string[] { "Robin", "Singh" }, Solution.StringToArray("Robin Singh"));
+        }
+
+        [Test]
+        public void _2_Example()
+        {
+            Assert.AreEqual(new string[] { "I", "love", "arrays", "they", "are", "my", "favorite" }, Solution.StringToArray("I love arrays they are my favorite"));
+        }
+    }
+}
