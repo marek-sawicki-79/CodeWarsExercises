3a6dada1aa42370232c5a18aeaef819ad9bd823f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-18 11:34:10 +0200

    done

--- RockPaperScissors/RockPaperScissors/Kata.cs
+++ RockPaperScissors/RockPaperScissors/Kata.cs
@@ -10,7 +10,18 @@ namespace RockPaperScissors
     {
         public string Rps(string p1, string p2)
         {
-            return "";
+            var O = "rock";
+            var X = "scissors";
+            var II = "paper";
+            if ((p1 == O && p2 == X) || (p1 == X && p2 == II) || (p1 == II && p2 == O))
+            {
+                return "Player 1 won!";
+            }
+            else if (p1 == p2)
+            {
+                return "Draw!";
+            }
+            else return "Player 2 won!";
         }
     }
 }
