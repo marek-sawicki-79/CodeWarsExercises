cba4c6f2ad0f3b06144c170f2eb2ed205f6c28fe
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-18 10:10:34 +0200

    exercise prep

---
+++ CountByX/CountByX/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountByX
+{
+    public class Kata
+    {
+        public static int[] CountBy(int x, int n)
+        {
+            int[] z = new int[];
+
+            return z;
+        }
+    }
+}
---
+++ CountByX/CountByX/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace CountByX
+{
+    [TestFixture]
+    public static class Test
+    {
+        [Test]
+        public static void CountBy1()
+        {
+            Assert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, Kata.CountBy(1, 5), "Array does not match");
+        }
+
+        [Test]
+        public static void CountBy2()
+        {
+            Assert.AreEqual(new int[] { 2, 4, 6, 8, 10 }, Kata.CountBy(2, 5), "Array does not match");
+        }
+    }
+}
