8a1e1fe62f57df6f05334b8366623d461901ce5c
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-20 12:10:16 +0200

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = "";
+do
+{
+    Console.WriteLine("enter number 1 to 100");
+    input = Console.ReadLine();
+    int x = int.Parse(input);
+    Console.WriteLine(((x % 3 == 0) && (x % 5 == 0)) ? "FizzBuzz" : (x % 3 == 0) ? "Fizz" : (x % 5 == 0) ? "Buzz" : x.ToString());
+} while (input != "x");
