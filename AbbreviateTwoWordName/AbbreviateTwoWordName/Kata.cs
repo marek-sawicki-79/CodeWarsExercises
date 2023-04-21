ae9ec5f64bcacf46f364d5d25b88a2fe3cd1b77b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-21 15:37:58 +0200

    exercise prep

---
+++ AbbreviateTwoWordName/AbbreviateTwoWordName/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbbreviateTwoWordName
+{
+    public class Kata
+    {
+        public static string AbbrevName(string name)
+        {
+            return "";
+        }
+    }
+}
