fa8a7d5eb7f123de8bd554dcf5d3c54dd7243438
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-21 23:30:01 +0200

    done

---
+++ CalculateAverage/CalculateAverage/AverageSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAverage
+{
+    class AverageSolution
+    {
+        public static double FindAverage(double[] array)
+        {
+            if (array.Length == 0) return 0;
+            else
+            {
+                double sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                return sum / array.Length;
+            }
+            //return (array.Length == 0) ? 0 : array.Average();
+        }
+    }
+}
