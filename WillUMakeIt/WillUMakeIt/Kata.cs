8c2ec98e177e06173c99d664c66a940b5b6f79ec
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-22 20:16:46 +0200

    done

---
+++ WillUMakeIt/WillUMakeIt/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WillUMakeIt
+{
+    public static class Kata
+    {
+        public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft)
+        {
+            return (distanceToPump <= mpg * fuelLeft) ? true : false;
+        }
+    }
+}
---
+++ WillUMakeIt/WillUMakeIt/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace WillUMakeIt
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, Kata.ZeroFuel(50, 25, 2));
+            Assert.AreEqual(false, Kata.ZeroFuel(100, 50, 1));
+        }
+    }
+}
