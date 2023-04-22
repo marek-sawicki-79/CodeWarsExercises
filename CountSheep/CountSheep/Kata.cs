bbb93b41ad7781f0b491cc05308cceaf25d6e2d7
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-22 23:09:11 +0200

    done

---
+++ CountSheep/CountSheep/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSheep
+{
+    public static class Kata
+    {
+        public static string CountSheep(int n)
+        {
+            string result = "";
+            for (int i = 1; i <= n; i++)
+            {
+               result += $"{i} sheep...";
+            }
+            return result;
+        }
+    }
+}
