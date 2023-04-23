fd1a9df0f473d497dd21cb0c1c9973f2b086e270
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-23 14:36:17 +0200

    done

---
+++ RemovingElements/RemovingElements/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovingElements
+{
+    public static class Kata
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+            var length = Math.Ceiling(arr.Length / 2.0);
+            var result = new object[(int)length];
+            var index = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result[index] = arr[i];
+                    index++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
