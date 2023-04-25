9be4c84f537f4487f28c7d47b6c157ead0f6a85e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-25 14:13:51 +0200

    done

---
+++ CorrectStringMistakes/CorrectStringMistakes/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectStringMistakes
+{
+    public class Kata
+    {
+        public static string Correct(string text)
+        {
+            Dictionary<char, char> fixMistake = new Dictionary<char, char>()
+            {
+                { '5', 'S' },
+                { '0', 'O' },
+                { '1', 'I' }
+
+            };
+            StringBuilder result = new StringBuilder();
+            foreach (char c in text)
+            {
+                    if (char.IsDigit(c))
+                    {
+                        result.Append(fixMistake[c]);
+                    }
+                    else result.Append(c);
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ CorrectStringMistakes/CorrectStringMistakes/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CorrectStringMistakes
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void CorrectTests()
+        {
+            Assert.AreEqual("LONDON", Kata.Correct("L0ND0N"));
+            Assert.AreEqual("DUBLIN", Kata.Correct("DUBL1N"));
+            Assert.AreEqual("SINGAPORE", Kata.Correct("51NGAP0RE"));
+            Assert.AreEqual("BUDAPEST", Kata.Correct("BUDAPE5T"));
+            Assert.AreEqual("PARIS", Kata.Correct("PAR15"));
+        }
+    }
+}
