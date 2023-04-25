72dfe9bab2fb18858da975eca5d3301e4cb7e0b4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-25 15:01:10 +0200

    done

---
+++ AlteringCase/AlteringCase/StringExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlteringCase
+{
+    public static class StringExt
+    {
+        public static string ToAlternatingCase(this string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    result += char.ToLower(s[i]);
+                }
+                else if (char.IsLower(s[i]))
+                {
+                    result += char.ToUpper(s[i]);
+                }
+                else result += s[i];
+            }
+            return result;
+        }
+    }
+}
