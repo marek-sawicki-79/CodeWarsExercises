62af5ce4fcde2dd2c23977c366efaf68415001f4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-26 17:21:33 +0200

    done

---
+++ SqareRootOrNot/SqareRootOrNot/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqareRootOrNot
+{
+    public class Kata
+    {
+        public static int[] SquareOrSquareRoot(int[] array)
+        {
+            int[] result = new int[array.Length];
+            double sqrt = 0.0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sqrt = Math.Sqrt(array[i]);
+                if (sqrt == (int)sqrt) result[i] = (int)sqrt;
+                else result[i] = (int)Math.Pow(array[i],2);
+            }
+            return result;
+        }
+    }
+}
---
+++ SqareRootOrNot/SqareRootOrNot/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace SqareRootOrNot
+{
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var input = new int[] { 4, 3, 9, 7, 2, 1 };
+            var expected = new int[] { 2, 9, 3, 49, 4, 1 };
+            Assert.AreEqual(string.Join(",", expected), string.Join(",", Kata.SquareOrSquareRoot(input)));
+
+            input = new int[] { 100, 101, 5, 5, 1, 1 };
+            expected = new int[] { 10, 10201, 25, 25, 1, 1 };
+            Assert.AreEqual(string.Join(",", expected), string.Join(",", Kata.SquareOrSquareRoot(input)));
+
+            input = new int[] { 1, 2, 3, 4, 5, 6 };
+            expected = new int[] { 1, 4, 9, 2, 25, 36 };
+            Assert.AreEqual(string.Join(",", expected), string.Join(",", Kata.SquareOrSquareRoot(input)));
+        }
+    }
+}
