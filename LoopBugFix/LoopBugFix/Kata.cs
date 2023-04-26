29d61b942eb1b1717ec771c8626ed87e34482075
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-26 17:08:56 +0200

    done

---
+++ LoopBugFix/LoopBugFix/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopBugFix
+{
+    public class Kata
+    {
+        public static List<int> CreateList(int number)
+        {
+            List<int> list = new List<int>();
+
+            for (int counter = 1; counter <= number; counter++)
+            {
+                list.Add(counter);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ LoopBugFix/LoopBugFix/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace LoopBugFix
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            List<int> myValues = new List<int>(new int[] { 1 });
+            Assert.AreEqual(myValues, Kata.CreateList(1));
+
+            myValues = new List<int>(new int[] { 1, 2 });
+            Assert.AreEqual(myValues, Kata.CreateList(2));
+
+            myValues = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(myValues, Kata.CreateList(6));
+        }
+    }
+}
