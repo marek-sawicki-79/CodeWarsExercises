233f0244a2ee4ed85867452a6248e70adf4f1100
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-28 14:11:23 +0200

    Done

--- DigitSqareConcat/DigitSqareConcat/Kata.cs
+++ DigitSqareConcat/DigitSqareConcat/Kata.cs
@@ -10,7 +10,17 @@ namespace DigitSqareConcat
     {
         public static int SquareDigits(int n)
         {
-            return 0;
+            char[] nArr = n.ToString().ToCharArray();
+            int[] cArr = new int[nArr.Length];
+            for (int i = 0; i < nArr.Length; i++)
+            {
+                int no = int.Parse(nArr[i].ToString());
+                cArr[i] = (int)Math.Pow(no, 2) ;
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach(int number in cArr) { sb.Append(number.ToString());}
+            int result = int.Parse(sb.ToString());
+            return result;
         }
     }
 }
