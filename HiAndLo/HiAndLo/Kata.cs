cf18eab069f48653c729b701d6294442833616dc
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-29 20:51:07 +0200

    converting string to int[] done

--- HiAndLo/HiAndLo/Kata.cs
+++ HiAndLo/HiAndLo/Kata.cs
@@ -10,7 +10,18 @@ namespace HiAndLo
     {
         public static string HighAndLow(string numbers)
         {
-            // Code here or
+            int[] numbersToo = new int[(numbers.Length + 1) / 2];
+            foreach (char number in numbers)
+            {
+                if (int.TryParse(number.ToString(), out int result))
+                {
+                    numbersToo.Append(result);
+                }
+                else
+                {
+                    break;
+                }
+            }
             return "throw towel";
         }
     }
