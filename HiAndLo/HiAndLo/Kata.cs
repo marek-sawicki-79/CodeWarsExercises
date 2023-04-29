d5ca870de944e19f5e98aaddd661319820b8eb0b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-29 20:41:50 +0200

    exercise prep

---
+++ HiAndLo/HiAndLo/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiAndLo
+{
+    public static class Kata
+    {
+        public static string HighAndLow(string numbers)
+        {
+            // Code here or
+            return "throw towel";
+        }
+    }
+}
---
+++ HiAndLo/HiAndLo/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace HiAndLo
+{
+    [TestFixture]
+    public class ExampleTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("42 -9", Kata.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("3 1", Kata.HighAndLow("1 2 3"));
+        }
+    }
+}
