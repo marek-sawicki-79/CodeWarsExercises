21c73ccd944adf0ee161f966b5b8599e0ae90151
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-01 22:42:58 +0200

    works but not for floats

---
+++ FindNextPerfectSqare/FindNextPerfectSqare/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNextPerfectSqare
+{
+    public class Kata
+    {
+        public static long FindNextSquare(long num)
+        {
+            double check = Math.Sqrt(num);
+            if (check != (long)check)
+            {
+                return -1;
+            }
+            else
+            {
+                do
+                {
+                    num++;
+                    check = Math.Sqrt(num);
+                }while(check != (long)check);
+                return num;
+            }
+        }
+    }
+}
---
+++ FindNextPerfectSqare/FindNextPerfectSqare/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace FindNextPerfectSqare
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(155, ExpectedResult = -1)]
+        [TestCase(121, ExpectedResult = 144)]
+        [TestCase(625, ExpectedResult = 676)]
+        [TestCase(319225, ExpectedResult = 320356)]
+        [TestCase(15241383936, ExpectedResult = 15241630849)]
+        public static long SampleTests(long num)
+        {
+            return Kata.FindNextSquare(num);
+        }
+    }
+}
