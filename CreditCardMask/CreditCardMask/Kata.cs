79b32c2aa2fbfabf6bb8bfa6dfde105d0e8b78cd
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-01 22:12:12 +0200

    exercise prep

---
+++ CreditCardMask/CreditCardMask/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardMask
+{
+    public static class Kata
+    {
+        public static string Maskify(string cc)
+        {
+        }
+    }
+}
---
+++ CreditCardMask/CreditCardMask/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace CreditCardMask
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ExamplesTests()
+        {
+            Assert.AreEqual("############5616", Kata.Maskify("4556364607935616"));
+            Assert.AreEqual("1", Kata.Maskify("1"));
+            Assert.AreEqual("#1111", Kata.Maskify("11111"));
+        }
+    }
+}
