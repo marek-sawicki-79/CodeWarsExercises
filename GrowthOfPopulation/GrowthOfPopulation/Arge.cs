a671edd1848458349db69360bd6b3f915d0f1725
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-02 21:59:02 +0200

    saving progress

---
+++ GrowthOfPopulation/GrowthOfPopulation/Arge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowthOfPopulation
+{
+    class Arge
+    {
+
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {
+            int result = 0;
+            double growth = p0 * percent * 0.01 + aug + p0;
+            result++;
+            if (growth < p)
+            {
+                growth = growth + growth * percent * 0.01 + aug * percent * 0.01 + aug;
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GrowthOfPopulation/GrowthOfPopulation/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace GrowthOfPopulation
+{
+    [TestFixture]
+    public static class ArgeTests
+    {
+
+        private static void testing(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Testing NbYear");
+            testing(Arge.NbYear(1500, 5, 100, 5000), 15);
+            testing(Arge.NbYear(1500000, 2.5, 10000, 2000000), 10);
+            testing(Arge.NbYear(1500000, 0.25, 1000, 2000000), 94);
+        }
+    }
+}
