8f0e6356257c9171ff8a6d3c1bef40717f383a35
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-03 12:54:41 +0200

    exercise prep

---
+++ StringEndsWith/StringEndsWith/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEndsWith
+{
+    public class Kata
+    {
+        public static bool Solution(string str, string ending)
+        {
+            // TODO: complete
+        }
+    }
+}
