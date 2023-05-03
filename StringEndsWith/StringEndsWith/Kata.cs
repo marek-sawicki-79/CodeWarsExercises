60ab508e7d8c0ee4d4952859328b5627f285ada4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-03 12:59:29 +0200

    done

--- StringEndsWith/StringEndsWith/Kata.cs
+++ StringEndsWith/StringEndsWith/Kata.cs
@@ -10,7 +10,7 @@ namespace StringEndsWith
     {
         public static bool Solution(string str, string ending)
         {
-            // TODO: complete
+            return str.EndsWith(ending);
         }
     }
 }
