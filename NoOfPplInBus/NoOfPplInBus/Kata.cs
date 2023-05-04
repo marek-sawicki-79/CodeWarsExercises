828074f4d799123fca2ecf378935421fd9a5e42b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-04 21:16:03 +0200

    exercise prep

---
+++ NoOfPplInBus/NoOfPplInBus/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoOfPplInBus
+{
+    public class Kata
+    {
+        public static int Number(List<int[]> peopleListInOut)
+        {
+            int peopleLeft = 0;
+            foreach (int[] people in peopleListInOut)
+            {
+
+            }
+        }
+    }
+}
