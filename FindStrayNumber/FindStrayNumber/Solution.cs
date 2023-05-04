ad3a72ccaec6bd2bce56179e7129d0caacdbaa66
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-04 22:06:54 +0200

    done

--- FindStrayNumber/FindStrayNumber/Solution.cs
+++ FindStrayNumber/FindStrayNumber/Solution.cs
@@ -10,7 +10,14 @@ namespace FindStrayNumber
     {
         public static int Stray(int[] numbers)
         {
-
+            int result = 0;
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                if (numbers[i] != numbers[i + 1] && numbers[i] != numbers[i + 2]) result = numbers[i];
+                else if (numbers[i + 1] != numbers[i] && numbers[i + 1] != numbers[i + 2]) result = numbers[i + 1];
+                else if (numbers[i + 2] != numbers[i + 1] && numbers[i + 2] != numbers[i]) result = numbers[i + 2];
+            }
+            return result;
         }
     }
 }
