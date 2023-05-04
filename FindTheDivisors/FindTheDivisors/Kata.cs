b39a36e79ac19c86b6c74b2db26e79178982fa47
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-04 21:41:01 +0200

    works (need to read instructions more thoroughly

--- FindTheDivisors/FindTheDivisors/Kata.cs
+++ FindTheDivisors/FindTheDivisors/Kata.cs
@@ -11,12 +11,12 @@ namespace FindTheDivisors
         public static int[] Divisors(int n)
         {
             List<int> divisors = new List<int>();
-            for (int i = 2; i < n; i++)
+            for (int i = 2; i <= n / 2; i++)
             {
-                if( n % i == 0 )
-                divisors.Add(i);
+                if( n % i == 0 ) divisors.Add(i);
             }
-            return divisors.ToArray();
+            if (divisors.Count == 0) return null;
+            else return divisors.ToArray();
         }
     }
 }
