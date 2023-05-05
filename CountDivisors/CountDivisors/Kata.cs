65712779979a97f4704be81dc0ff439532cd3bcf
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-05 10:18:10 +0200

    done

--- CountDivisors/CountDivisors/Kata.cs
+++ CountDivisors/CountDivisors/Kata.cs
@@ -10,7 +10,16 @@ namespace CountDivisors
     {
         public static int Divisors(int n)
         {
-            // todo
+            List<int> divisor = new List<int>();
+            for(int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisor.Add(i);
+                }
+            }
+            int result = divisor.Count;
+            return result;
         }
     }
 }
