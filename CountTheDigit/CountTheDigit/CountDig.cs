94069bfaaf9ac53b9c5d2d8db19d241e17b2128a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-05 10:56:59 +0200

    exercise prep

---
+++ CountTheDigit/CountTheDigit/CountDig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTheDigit
+{
+    public class CountDig
+    {
+
+        public static int NbDig(int n, int d)
+        {
+            // your code
+        }
+    }
+}
