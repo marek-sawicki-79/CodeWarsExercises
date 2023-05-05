0a0136668e39673dd058a93eb1d3bf52661015be
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-05 10:47:49 +0200

    covered inputs < 0 done

--- BreakingChocolate/BreakingChocolate/Kata.cs
+++ BreakingChocolate/BreakingChocolate/Kata.cs
@@ -10,7 +10,8 @@ namespace BreakingChocolate
     {
         public static int BreakChocolate(int n, int m)
         {
-            if (n <= 1 && m <= 1) return 0;
+            if (n == 1 && m == 1) return 0;
+            else if (n <= 0 || m <= 0) return 0;
             else if (n == 1) return m - 1;
             else if (m == 1) return n - 1;
             else return (n - 1) + n * (m - 1);
