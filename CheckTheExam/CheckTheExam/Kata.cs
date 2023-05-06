e7f40bb43881c64c5a3d08db0d97e649e4d8880a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-06 20:37:56 +0200

    done

--- CheckTheExam/CheckTheExam/Kata.cs
+++ CheckTheExam/CheckTheExam/Kata.cs
@@ -10,7 +10,24 @@ namespace CheckTheExam
     {
         public static int CheckExam(string[] arr1, string[] arr2)
         {
-
+            int result = 0;
+            for (int i = 0; i < arr1.Length && i < arr2.Length; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    result += 4;
+                }
+                else if (arr2[i] == "")
+                {
+                    continue;
+                }
+                else if (arr1[i] != arr2[i])
+                {
+                    result -= 1;
+                }
+            }
+            if (result < 0) return 0;
+            else return result;
         }
     }
 }
