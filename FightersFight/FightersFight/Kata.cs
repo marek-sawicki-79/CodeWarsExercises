6f4cba80766b27c2580b49f9bba47abc5f590986
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-08 21:14:12 +0200

    done

--- FightersFight/FightersFight/Kata.cs
+++ FightersFight/FightersFight/Kata.cs
@@ -14,17 +14,21 @@ namespace FightersFight
             {
                 if (fighter1.Name == firstAttacker)
                 {
-                    fighter2.Health = fighter2.Health - fighter1.DamagePerAttack;
-                    fighter1.Health = fighter1.Health - fighter2.DamagePerAttack;
+                    if (fighter1.Health > 0) fighter2.Health = fighter2.Health - fighter1.DamagePerAttack;
+                    else break;
+                    if (fighter2.Health > 0) fighter1.Health = fighter1.Health - fighter2.DamagePerAttack;
+                    else break;
                 }
                 else
                 {
-                    fighter1.Health = fighter1.Health - fighter2.DamagePerAttack;
-                    fighter2.Health = fighter2.Health - fighter1.DamagePerAttack;
+                    if (fighter2.Health > 0) fighter1.Health = fighter1.Health - fighter2.DamagePerAttack;
+                    else break;
+                    if (fighter2.Health > 0) fighter2.Health = fighter2.Health - fighter1.DamagePerAttack;
+                    else break;
                 }
-            }while (fighter1.Health <= 0 || fighter2.Health <= 0);
-            if (fighter1.Health <= 0) return fighter2.Name;
-            else return fighter1.Name;
+            }while (fighter1.Health > 0 || fighter2.Health > 0);
+            if (fighter1.Health > 0) return fighter1.Name;
+            else return fighter2.Name;
         }
     }
 }
