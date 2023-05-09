524474012ec88454996c36c49f2bb2739f3751ee
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-09 11:56:33 +0200

    exercise prep

---
+++ CheckIfNumbersAreInOrderAsc/CheckIfNumbersAreInOrderAsc/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfNumbersAreInOrderAsc
+{
+    public class Kata
+    {
+        public static bool IsAscOrder(int[] arr)
+        { /* your code here */}
+    }
+}
