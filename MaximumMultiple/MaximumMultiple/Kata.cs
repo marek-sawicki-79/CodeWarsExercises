d33238f5d04990a7418d6d5592ee1a607ab27461
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-09 11:23:05 +0200

    exercise prep

---
+++ MaximumMultiple/MaximumMultiple/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumMultiple
+{
+    public class Kata
+    {
+        public static int MaxMultiply(int divisor, int bound)
+        {
+            //Do some magic
+        }
+    }
+}
