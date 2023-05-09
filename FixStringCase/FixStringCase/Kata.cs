8c21289ed6599053d47ef2e6d354280c70f573ad
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-09 11:46:51 +0200

    exercise prep

---
+++ FixStringCase/FixStringCase/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixStringCase
+{
+    class Kata
+    {
+        public static string Solve(string s)
+        {
+            //Do Some Magic
+        }
+    }
+}
