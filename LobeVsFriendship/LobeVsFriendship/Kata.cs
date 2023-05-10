48c4a5707622f5e6168e6794e681fb8673d4ce91
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-10 17:41:59 +0200

    exercise prep

---
+++ LobeVsFriendship/LobeVsFriendship/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LobeVsFriendship
+{
+    public static class Kata
+    {
+        public static int WordsToMarks(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
