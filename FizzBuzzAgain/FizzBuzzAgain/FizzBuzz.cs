c3826a2344fcf3f6d73d21949c7f68d021bf35c0
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-10 20:26:06 +0200

    exercise prep

---
+++ FizzBuzzAgain/FizzBuzzAgain/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzAgain
+{
+	public class FizzBuzz
+	{
+		public static string[] GetFizzBuzzArray(int n)
+		{
+			return Array.Empty<string>();
+		}
+	}
+}
