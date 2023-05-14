8bf487f2dbf92c584fbc6401ce8c364a5185ecc6
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-14 20:38:09 +0200

    exercise prep

---
+++ SumFromZeroToN/SumFromZeroToN/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SumFromZeroToN
+{
+    [TestFixture]
+    public class SequenceSumTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("0+1+2+3+4+5+6 = 21", SequenceSum.ShowSequence(6));
+        }
+    }
+}
