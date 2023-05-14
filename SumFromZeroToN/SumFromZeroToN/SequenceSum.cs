e27fa16a16714a1ec2265c2e8a3ff31ce0e1dc3e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-14 20:59:12 +0200

    Concat method wrong use

--- SumFromZeroToN/SumFromZeroToN/SequenceSum.cs
+++ SumFromZeroToN/SumFromZeroToN/SequenceSum.cs
@@ -10,7 +10,19 @@ namespace SumFromZeroToN
     {
         public static string ShowSequence(int n)
         {
-            return "";
+            if (n < 0) return $"{n}<0";
+            else if (n == 0) return "0=0";
+            else
+            {
+                string result = "0";
+                int sum = 0;
+                for(int i = 1; i <= n; i++)
+                {
+                    sum += i;
+                    result.Concat($"+{i}");
+                }
+                return result + $" = {sum}";
+            }
         }
     }
 }
