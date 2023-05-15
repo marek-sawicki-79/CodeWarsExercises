ea075d23da5d827f957f48ca503b21381321e65c
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-15 19:21:19 +0200

    exercise prep

---
+++ CubedSum/CubedSum/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubedSum
+{
+    public static class Kata
+    {
+        public static long SumCubes(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
