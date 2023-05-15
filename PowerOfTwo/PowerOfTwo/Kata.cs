cdffc12795b49b8a0fa5bbe3a95680e9cfab8e6e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-15 19:27:24 +0200

    exercise prep

---
+++ PowerOfTwo/PowerOfTwo/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfTwo
+{
+    public static class Kata
+    {
+        public static bool PowerOfTwo(int n)
+        {
+            //Math --> Magic
+        }
+    }
+}
---
+++ PowerOfTwo/PowerOfTwo/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace PowerOfTwo
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(2).Returns(true);
+                yield return new TestCaseData(4096).Returns(true);
+                yield return new TestCaseData(5).Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool SampleTest(int n) => Kata.PowerOfTwo(n);
+    }
+}
