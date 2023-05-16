a1661316d8920303d9400d80de599fca45df6780
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-16 20:08:14 +0200

    same solution w_o array, using string and ternary operator

--- FindNthDigit/FindNthDigit/Kata.cs
+++ FindNthDigit/FindNthDigit/Kata.cs
@@ -10,11 +10,14 @@ namespace FindNthDigit
     {
         public static int FindDigit(int num, int nth)
         {
-            char[] numStrings = num.ToString().ToCharArray();
+            //char[] numStrings = num.ToString().ToCharArray();
+            //if (nth <= 0) return -1;
+            //else if (nth > numStrings.Length) return 0;
+            //else if (!char.IsDigit(numStrings[numStrings.Length - nth])) return 0;
+            //else return int.Parse(numStrings[numStrings.Length - nth].ToString());
             if (nth <= 0) return -1;
-            else if (nth > numStrings.Length) return 0;
-            else if (!char.IsDigit(numStrings[numStrings.Length - nth])) return 0;
-            else return int.Parse(numStrings[numStrings.Length - nth].ToString());
+            var str = Math.Abs(num).ToString();
+            return nth > str.Length ? -0 : int.Parse(str[str.Length - nth].ToString());
         }
     }
 }
