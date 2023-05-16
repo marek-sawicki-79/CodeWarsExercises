5aaa429eff891a3c9593596d76084dfb6a0aaf78
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-16 20:03:37 +0200

    done

--- FindNthDigit/FindNthDigit/Kata.cs
+++ FindNthDigit/FindNthDigit/Kata.cs
@@ -10,7 +10,11 @@ namespace FindNthDigit
     {
         public static int FindDigit(int num, int nth)
         {
-
+            char[] numStrings = num.ToString().ToCharArray();
+            if (nth <= 0) return -1;
+            else if (nth > numStrings.Length) return 0;
+            else if (!char.IsDigit(numStrings[numStrings.Length - nth])) return 0;
+            else return int.Parse(numStrings[numStrings.Length - nth].ToString());
         }
     }
 }
