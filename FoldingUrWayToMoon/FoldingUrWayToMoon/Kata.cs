a0b27149dacff9827d08d6f4578d06ff7ed8ccc4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-18 22:01:38 +0200

    done

--- FoldingUrWayToMoon/FoldingUrWayToMoon/Kata.cs
+++ FoldingUrWayToMoon/FoldingUrWayToMoon/Kata.cs
@@ -10,7 +10,18 @@ namespace FoldingUrWayToMoon
     {
         public static int? FoldTo(double distance)
         {
-
+            if (distance < 0) return null;
+            else
+            {
+                double thickness = 0.0001;
+                int folds = 0;
+                while(thickness < distance)
+                {
+                    folds++;
+                    thickness = thickness * 2;
+                }
+                return folds;
+            }
         }
     }
 }
