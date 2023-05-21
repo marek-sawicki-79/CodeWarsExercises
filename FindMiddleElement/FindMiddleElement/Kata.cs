f594c727217c6a0770365fc801ce262d80f0d05d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-21 20:44:53 +0200

    exercise prep

---
+++ FindMiddleElement/FindMiddleElement/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMiddleElement
+{
+    public class Kata
+    {
+        public static int Gimme(double[] inputArray)
+        {
+            // Implement this function
+        }
+    }
+}
---
+++ FindMiddleElement/FindMiddleElement/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace FindMiddleElement
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual(0, Kata.Gimme(new double[] { 2, 3, 1 }));
+            Assert.AreEqual(1, Kata.Gimme(new double[] { 5, 10, 14 }));
+        }
+    }
+}
