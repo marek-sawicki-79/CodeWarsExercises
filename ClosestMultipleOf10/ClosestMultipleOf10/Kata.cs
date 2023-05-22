7f50b9eb25a94185e0725078addce50e523088ee
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-22 21:10:17 +0200

    exercise prep

---
+++ ClosestMultipleOf10/ClosestMultipleOf10/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosestMultipleOf10
+{
+    public class Kata
+    {
+        public int ClosestMultiple10(int num)
+        {
+            return num;
+        }
+    }
+}
