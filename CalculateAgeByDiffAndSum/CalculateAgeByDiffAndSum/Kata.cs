874e1de2c746f7fcf612c99971700b4efd48e19c
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-23 23:07:20 +0200

    k proste

--- CalculateAgeByDiffAndSum/CalculateAgeByDiffAndSum/Kata.cs
+++ CalculateAgeByDiffAndSum/CalculateAgeByDiffAndSum/Kata.cs
@@ -10,13 +10,16 @@ namespace CalculateAgeByDiffAndSum
     {
         public static double[] GetAges(int sum, int difference)
         {
+            if (sum < 0 || difference < 0)
+                return null;
+
             double[] result = new double[2];
-            if (sum < 0 || difference < 0) return null;
-            else
-            {
-                result[1] = (sum -difference) / 2.0;
-                result[0] = difference + result[1];
-            }
+            result[1] = (sum - difference) / 2.0;
+            result[0] = difference + result[1];
+
+            if (result[0] < 0 || result[1] < 0)
+                return null;
+
             return result;
         }
     }
