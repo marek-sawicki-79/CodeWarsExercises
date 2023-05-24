ce264619cbe11acc65fc37c5bdca8d7552a98079
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-24 21:42:28 +0200

    done

--- SumOfDigits_DigitalRoot/SumOfDigits_DigitalRoot/Number.cs
+++ SumOfDigits_DigitalRoot/SumOfDigits_DigitalRoot/Number.cs
@@ -10,8 +10,19 @@ namespace SumOfDigits_DigitalRoot
     {
         public static int DigitalRoot(long n)
         {
-            // Your awesome code here!
-            return 0;
+            List<int> digits = new List<int>();
+            while (n > 9)
+            {
+                string nstr = n.ToString();
+                digits.Clear();
+                foreach(char c in nstr)
+                {
+                    int digit = int.Parse(c.ToString());
+                    digits.Add(digit);
+                }
+                n = digits.Sum();
+            }
+            return (int)n;
         }
     }
 }
