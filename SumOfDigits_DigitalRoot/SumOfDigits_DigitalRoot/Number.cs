763867864cfe901f6b185b6bd396c739a403fc25
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-24 21:27:01 +0200

    exercise prep

---
+++ SumOfDigits_DigitalRoot/SumOfDigits_DigitalRoot/Number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits_DigitalRoot
+{
+    public class Number
+    {
+        public static int DigitalRoot(long n)
+        {
+            // Your awesome code here!
+            return 0;
+        }
+    }
+}
