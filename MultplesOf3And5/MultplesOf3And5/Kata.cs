d010965eb191850c02cd4d22d9d1dff8d941a92e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-24 21:03:40 +0200

    exercise prep

---
+++ MultplesOf3And5/MultplesOf3And5/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultplesOf3And5
+{
+    public static class Kata
+    {
+        public static int Solution(int value)
+        {
+            // Magic Happens
+        }
+    }
+}
---
+++ MultplesOf3And5/MultplesOf3And5/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace MultplesOf3And5
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, Kata.Solution(10));
+        }
+    }
+}
