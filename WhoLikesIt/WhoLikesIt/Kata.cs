7c36c98e7893116ba340e037086f6bc87e99bb2c
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-24 21:46:31 +0200

    exercise prep

---
+++ WhoLikesIt/WhoLikesIt/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoLikesIt
+{
+    public static class Kata
+    {
+        public static string Likes(string[] name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
