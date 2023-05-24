17fb292fd40536cfe8bec75b302032d482121095
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-24 22:33:54 +0200

    solution from codewars using switch/case

--- WhoLikesIt/WhoLikesIt/Kata.cs
+++ WhoLikesIt/WhoLikesIt/Kata.cs
@@ -10,11 +10,21 @@ namespace WhoLikesIt
     {
         public static string Likes(string[] name)
         {
-            if (name.Length == 0) return "no one likes this";
-            else if (name.Length == 1) return $"{name[0]} likes this";
-            else if (name.Length == 2) return $"{name[0]} and {name[1]} like this";
-            else if (name.Length == 3) return $"{name[0]}, {name[1]} and {name[2]} like this";
-            else return $"{name[0]}, {name[1]} and {name.Count() - 2} others like this";
+            //if (name.Length == 0) return "no one likes this";
+            //else if (name.Length == 1) return $"{name[0]} likes this";
+            //else if (name.Length == 2) return $"{name[0]} and {name[1]} like this";
+            //else if (name.Length == 3) return $"{name[0]}, {name[1]} and {name[2]} like this";
+            //else return $"{name[0]}, {name[1]} and {name.Count() - 2} others like this";
+
+            switch (name.Length)
+            {
+                case 0: return "no one likes this";
+                case 1: return $"{name[0]} likes this";
+                case 2: return $"{name[0]} and {name[1]} like this";
+                case 3: return $"{name[0]}, {name[1]} and {name[2]} like this";
+                default:
+                    return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+            }
         }
     }
 }
