ac9fbe19b7a14741816eea7119e136f41be5f678
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-25 20:58:12 +0200

    done

--- ArrayDiff/ArrayDiff/Kata.cs
+++ ArrayDiff/ArrayDiff/Kata.cs
@@ -10,8 +10,27 @@ namespace ArrayDiff
     {
         public static int[] ArrayDiff(int[] a, int[] b)
         {
-            // Your brilliant solution goes here
-            // It's possible to pass random tests in about a second ;)
+            List<int> result = new List<int>();
+            foreach( int num in a)
+            {
+                if(!ArrayContains(b, num))
+                {
+                    result.Add(num);
+                }
+            }
+            return result.ToArray();
+        }
+
+        private static bool ArrayContains(int[] array, int piece)
+        {
+            foreach( int num in array)
+            {
+                if(num == piece)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
