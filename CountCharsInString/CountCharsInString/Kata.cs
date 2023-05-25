73f4c482e36431fda44fd68f497441266881e9d7
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-25 21:45:06 +0200

    done

---
+++ CountCharsInString/CountCharsInString/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountCharsInString
+{
+    public class Kata
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+            foreach(char c in str)
+            {
+                if (letters.ContainsKey(c))
+                {
+                    letters[c]++;
+                }
+                else
+                {
+                    letters[c] = 1;
+                }
+            }
+            return letters;
+        }
+    }
+}
