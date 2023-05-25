1ca09d0a8c6c901a9bffc8e854a5805a582a40c4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-25 21:07:19 +0200

    solution from codewars using String.Concat

--- PhoneNumberCreator/PhoneNumberCreator/Kata.cs
+++ PhoneNumberCreator/PhoneNumberCreator/Kata.cs
@@ -10,9 +10,10 @@ namespace PhoneNumberCreator
     {
         public static string CreatePhoneNumber(int[] numbers)
         {
-            return $"({numbers[0]}{numbers[1]}{numbers[2]}) " +
-                $"{numbers[3]}{numbers[4]}{numbers[5]}-" +
-                $"{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+            //return $"({numbers[0]}{numbers[1]}{numbers[2]}) " +
+            //    $"{numbers[3]}{numbers[4]}{numbers[5]}-" +
+            //    $"{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+            return long.Parse(string.Concat(numbers)).ToString("(000) 000-0000");
         }
     }
 }
