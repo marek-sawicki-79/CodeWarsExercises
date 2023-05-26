b02859f4a7b869b496bd4fb3cb8c1217bf9ed007
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-26 20:46:47 +0200

    exercise prep

---
+++ IsItPrime/IsItPrime/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItPrime
+{
+    public static class Kata
+    {
+        public static bool IsPrime(int n)
+        {
+            return false; // TODO
+        }
+    }
+}
---
+++ IsItPrime/IsItPrime/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace IsItPrime
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static IEnumerable<TestCaseData> sampleTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(0).Returns(false);
+                yield return new TestCaseData(1).Returns(false);
+                yield return new TestCaseData(2).Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource("sampleTestCases")]
+        public bool SampleTest(int n) => Kata.IsPrime(n);
+    }
+}
+}
