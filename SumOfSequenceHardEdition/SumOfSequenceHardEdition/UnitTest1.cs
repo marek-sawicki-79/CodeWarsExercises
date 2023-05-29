f9e717597cb413a696065764ba1595298e1ab509
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-29 20:37:25 +0200

    forgot about tests

--- SumOfSequenceHardEdition/SumOfSequenceHardEdition/UnitTest1.cs
+++ SumOfSequenceHardEdition/SumOfSequenceHardEdition/UnitTest1.cs
@@ -1,16 +1,19 @@
 namespace SumOfSequenceHardEdition
 {
-    public class Tests
+    [TestFixture]
+    public class SolutionTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void BasicTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(12, Kata.SequenceSum(2, 6, 2));
+            Assert.AreEqual(15, Kata.SequenceSum(1, 5, 1));
+            Assert.AreEqual(5, Kata.SequenceSum(1, 5, 3));
+            Assert.AreEqual(-5, Kata.SequenceSum(-1, -5, -3));
+            Assert.AreEqual(0, Kata.SequenceSum(16, 15, 3));
+            Assert.AreEqual(-26, Kata.SequenceSum(-24, -2, 22));
+            Assert.AreEqual(-2, Kata.SequenceSum(-2, 4, 658));
+            Assert.AreEqual(469436517521190, Kata.SequenceSum(780, 68515438, 5));
         }
     }
 }
