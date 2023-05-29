c6a701bd73c5a0c94920dc4327bf73f861e54519
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-29 20:30:31 +0200

    exercise prep

---
+++ SumOfSequenceHardEdition/SumOfSequenceHardEdition/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfSequenceHardEdition
+{
+    public static class Kata
+    {
+        public static long SequenceSum(long start, long end, long step)
+        {
+            return -1;
+        }
+    }
+}
