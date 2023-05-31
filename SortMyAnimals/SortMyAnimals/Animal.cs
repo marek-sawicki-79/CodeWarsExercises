44ef1b57cfdfcb374225b93b330177b5494c1070
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-31 22:18:05 +0200

    works

---
+++ SortMyAnimals/SortMyAnimals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortMyAnimals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int NumberOfLegs { get; set; }
+    }
+
+    public class AnimalSorter
+    {
+        public List<Animal> Sort(List<Animal> input)
+        {
+            return input.OrderBy(a => a.NumberOfLegs).ThenBy(b => b.Name).ToList();
+        }
+    }
+}
