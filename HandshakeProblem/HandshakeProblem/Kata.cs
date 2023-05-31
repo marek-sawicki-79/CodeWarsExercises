82c8a9a71258231cef351f63b1bce11701aaf104
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-31 21:50:11 +0200

    dooo

--- HandshakeProblem/HandshakeProblem/Kata.cs
+++ HandshakeProblem/HandshakeProblem/Kata.cs
@@ -10,7 +10,10 @@ namespace HandshakeProblem
     {
         public static int GetParticipants(int handshakes)
         {
-            throw new NotImplementedException();
+            if (handshakes <= 0) return 0;
+            if (handshakes == 1) return 2;
+            double participants = Math.Ceiling((Math.Sqrt(8 * handshakes + 1) - 1) / 2);
+            return (int)participants;
         }
     }
 }
