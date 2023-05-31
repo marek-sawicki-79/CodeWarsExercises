d09a1a274b6f1907ba20762af6049e4231a73202
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-05-31 22:03:52 +0200

    this also worked

--- HandshakeProblem/HandshakeProblem/Kata.cs
+++ HandshakeProblem/HandshakeProblem/Kata.cs
@@ -10,11 +10,12 @@ namespace HandshakeProblem
     {
         public static int GetParticipants(int handshakes)
         {
-            if (handshakes <= 0) return 0;
-            int k;
-            int i;
-            for ( i = 0, k = 1; i < handshakes; i += k++) ;
-            return k;
+            //if (handshakes <= 0) return 0;
+            //int k;
+            //int i;
+            //for ( i = 0, k = 1; i < handshakes; i += k++) ;
+            //return k;
+            return handshakes == 0 ? 0 : (int)Math.Ceiling((1 + Math.Sqrt(1 + 8 * handshakes)) / 2.0);
         }
     }
 }
