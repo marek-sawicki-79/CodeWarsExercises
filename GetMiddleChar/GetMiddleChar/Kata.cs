7d5395d561bea506289179112c10803260deb1b0
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-02 22:00:45 +0200

    done

--- GetMiddleChar/GetMiddleChar/Kata.cs
+++ GetMiddleChar/GetMiddleChar/Kata.cs
@@ -10,7 +10,18 @@ namespace GetMiddleChar
     {
         public static string GetMiddle(string s)
         {
-            //Code goes here!
+            if(s.Length % 2 == 0)
+            {
+                char[] chars = new char[2];
+                chars[0] = s[s.Length / 2 - 1];
+                chars[1] = s[s.Length / 2];
+                return new string(chars);
+            }
+            else
+            {
+                int middleIndex = (int)Math.Floor(s.Length / 2.0);
+                return s[middleIndex].ToString();
+            }
         }
     }
 }
