7c315bf65a10c95b2bfb46eaeb669c17f56059a9
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-02 20:55:22 +0200

    exercise prep

---
+++ DescendingOrder/DescendingOrder/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescendingOrder
+{
+    public static class Kata
+    {
+        public static int DescendingOrder(int num)
+        {
+            // Bust a move right here
+            return 0;
+        }
+    }
+}
---
+++ DescendingOrder/DescendingOrder/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace DescendingOrder
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test0()
+        {
+            Assert.AreEqual(0, Kata.DescendingOrder(0));
+        }
+    }
+}
