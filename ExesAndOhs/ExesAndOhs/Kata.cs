9c724f51e45a874cd25bf8350d052c1c9ceaa5e2
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-03 23:12:55 +0200

    exercise prep

---
+++ ExesAndOhs/ExesAndOhs/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExesAndOhs
+{
+    public static class Kata
+    {
+        public static bool XO(string input)
+        {
+            return //Code it!
+  }
+    }
+}
---
+++ ExesAndOhs/ExesAndOhs/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ExesAndOhs
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(true, Kata.XO("xo"));
+            Assert.AreEqual(true, Kata.XO("xxOo"));
+            Assert.AreEqual(false, Kata.XO("xxxm"));
+            Assert.AreEqual(false, Kata.XO("Oo"));
+            Assert.AreEqual(false, Kata.XO("ooom"));
+        }
+    }
+}
