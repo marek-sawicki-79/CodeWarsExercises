513ea801f1d10eec2977bbd16f31ef82f6275d77
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-03 23:09:02 +0200

    done

--- ListFiltering/ListFiltering/ListFilterer.cs
+++ ListFiltering/ListFiltering/ListFilterer.cs
@@ -10,7 +10,15 @@ namespace ListFiltering
     {
         public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
         {
-
+            List<int> result = new List<int>();
+            foreach(object item in listOfItems)
+            {
+                if(item is int)
+                {
+                    result.Add((int)item);
+                }
+            }
+            return result;
         }
     }
 
