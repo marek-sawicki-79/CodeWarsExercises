21130ebe269715c4dc57ce9ea4a098f427614367
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-04 19:23:32 +0200

    exercise prep

---
+++ ComplementaryDNA/ComplementaryDNA/DnaStrand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplementaryDNA
+{
+    public class DnaStrand
+    {
+        public static string MakeComplement(string dna)
+        {
+            //Your code
+        }
+    }
+}
---
+++ ComplementaryDNA/ComplementaryDNA/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace ComplementaryDNA
+{
+    [TestFixture]
+    public class DnaStrandTest
+    {
+        [TestCase("AAAA", "TTTT")]
+        [TestCase("ATTGC", "TAACG")]
+        [TestCase("GTAT", "CATA")]
+        [TestCase("AAGG", "TTCC")]
+        [TestCase("CGCG", "GCGC")]
+        [TestCase("ATTGC", "TAACG")]
+        [TestCase("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA", "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT")]
+        public void SampleTests(string dna, string expected)
+        {
+            Assert.AreEqual(expected, DnaStrand.MakeComplement(dna));
+        }
+    }
+}
