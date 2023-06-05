8820bdd3ec486821374511edb6125f034da85fc5
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-05 12:18:40 +0200

    cleaner

--- TwoToOne/TwoToOne/TwoToOne.cs
+++ TwoToOne/TwoToOne/TwoToOne.cs
@@ -11,11 +11,13 @@ namespace TwoToOne
 
         public static string Longest(string s1, string s2)
         {
-            string distinctS1 = new string(s1.Distinct().ToArray());
-            string distinctS2 = new string(s2.Distinct().ToArray());
-            string stringSum = distinctS1 + distinctS2;
-            char[] sorted = stringSum.Distinct().OrderBy(x => x).ToArray();
-            return new string(sorted);
+            //string distinctS1 = new string(s1.Distinct().ToArray());
+            //string distinctS2 = new string(s2.Distinct().ToArray());
+            //string stringSum = distinctS1 + distinctS2;
+            //char[] sorted = stringSum.Distinct().OrderBy(x => x).ToArray();
+            //return new string(sorted);
+
+            return new string((s1 + s2).Distinct().OrderBy(x => x).ToArray());
         }
     }
 }
