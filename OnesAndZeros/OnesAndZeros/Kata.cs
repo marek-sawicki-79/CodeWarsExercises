ef1e5845cba2b0707cd303120980aadc6c96328a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-05 12:24:42 +0200

    exercise prep

---
+++ OnesAndZeros/OnesAndZeros/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnesAndZeros
+{
+    class Kata
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            //Code here
+        }
+    }
+}
