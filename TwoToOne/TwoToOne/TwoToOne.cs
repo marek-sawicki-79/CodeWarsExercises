360729f4d4881cd284600f30804bb1dba49ea1d5
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-05 12:08:25 +0200

    exercise prep

---
+++ TwoToOne/TwoToOne/TwoToOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoToOne
+{
+    public class TwoToOne
+    {
+
+        public static string Longest(string s1, string s2)
+        {
+            // your code
+        }
+    }
+}
---
+++ TwoToOne/TwoToOne/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TwoToOne
+{
+    [TestFixture]
+    public static class TwoToOneTests
+    {
+
+        [Test]
+        public static void test1()
+        {
+            Assert.AreEqual("aehrsty", TwoToOne.Longest("aretheyhere", "yestheyarehere"));
+            Assert.AreEqual("abcdefghilnoprstu", TwoToOne.Longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+            Assert.AreEqual("acefghilmnoprstuy", TwoToOne.Longest("inmanylanguages", "theresapairoffunctions"));
+        }
+    }
+}
