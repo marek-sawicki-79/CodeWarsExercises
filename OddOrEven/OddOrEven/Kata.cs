ec0cd23bea447473fab98f2c2afedeb50593b749
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-07 20:18:20 +0200

    exercise prep

---
+++ OddOrEven/OddOrEven/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    public class Kata
+    {
+        public static string OddOrEven(int[] array)
+        {
+            //enter code here
+        }
+    }
+}
