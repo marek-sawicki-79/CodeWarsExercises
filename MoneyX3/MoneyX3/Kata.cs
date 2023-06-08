a20ba7739ee2c4db9dfa79853cf4db2ce628ab1d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-08 21:41:36 +0200

    done

--- MoneyX3/MoneyX3/Kata.cs
+++ MoneyX3/MoneyX3/Kata.cs
@@ -10,6 +10,12 @@ namespace MoneyX3
     {
         public static int CalculateYears(double principal, double interest, double tax, double desiredPrincipal)
         {
+            int years = 0;
+            while(principal < desiredPrincipal)
+            {
+                principal = principal + (principal * interest) - (principal * interest * tax);
+                years++;
+            }
             return years;
         }
     }
