1c7734917d05a704d1f21eeabadf9b03de84f4d8
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-09 18:14:49 +0200

    exercise prep

---
+++ RoundUpTo5/RoundUpTo5/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundUpTo5
+{
+    public class Kata
+    {
+        public static int RoundToNext5(int n)
+        {
+            //Your code goes here...
+        }
+    }
+}
