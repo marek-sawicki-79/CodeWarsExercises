823b732c79b91d86ad8decb792551e663f618467
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-11 14:12:19 +0200

    done

--- AlternateCapitalization/AlternateCapitalization/Kata.cs
+++ AlternateCapitalization/AlternateCapitalization/Kata.cs
@@ -10,8 +10,24 @@ namespace AlternateCapitalization
     {
         public static string[] Capitalize(string s)
         {
+            string[] result = new string[2];
+            char[] sChars1 = s.ToCharArray();
+            char[] sChars2 = s.ToCharArray();
 
-            throw new NotImplementedException();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    sChars1[i] = char.ToUpper(s[i]);
+                }
+                else if(i % 2 != 0)
+                {
+                    sChars2[i] = char.ToUpper(s[i]);
+                }
+            }
+            result[0] = new string(sChars1);
+            result[1] = new string(sChars2);
+            return result;
         }
     }
 }
