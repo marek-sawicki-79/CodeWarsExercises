100c76220ef8f6a516757e3ab58e7058bdc0da5d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-12 19:44:11 +0200

    done

--- RowWeights/RowWeights/Kata.cs
+++ RowWeights/RowWeights/Kata.cs
@@ -10,7 +10,23 @@ namespace RowWeights
     {
         public static int[] RowWeights(int[] a)
         {
-            //Do Some Magic
+            int[] result = new int[2];
+            int sumOdd = 0;
+            int sumEven = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    sumEven += a[i];
+                }
+                else
+                {
+                    sumOdd += a[i];
+                }
+            }
+            result[0] = sumEven;
+            result[1] = sumOdd;
+            return result;
         }
     }
 }
