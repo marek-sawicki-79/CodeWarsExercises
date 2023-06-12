7f09744df4bf97ab8882ad38df659af7f25d5b22
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-12 19:57:29 +0200

    cleaner

--- FactorialUpANotch/FactorialUpANotch/Program.cs
+++ FactorialUpANotch/FactorialUpANotch/Program.cs
@@ -8,18 +8,18 @@ namespace FactorialUpANotch
 {
     public static class Program
     {
-        public static ulong Factorial(int n)
-        {
-            if(n == 0) return 1;
-            else
-            {
-                ulong result = 1;
-                for (int i = 1; i <= n; i++)
-                {
-                    result *= (ulong)i;
-                }
-                return result;
-            }
-        }
+        public static int Factorial(int n) => (n == 0) ? 1 : n * Factorial(n - 1);
+        //{
+        //    if(n == 0) return 1;
+        //    else
+        //    {
+        //        ulong result = 1;
+        //        for (int i = 1; i <= n; i++)
+        //        {
+        //            result *= (ulong)i;
+        //        }
+        //        return result;
+        //    }
+        //}
     }
 }
