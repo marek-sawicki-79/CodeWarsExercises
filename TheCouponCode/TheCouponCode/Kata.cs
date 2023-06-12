c54788324c50f0c349134481941d8d47b14b239a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-12 19:44:51 +0200

    done

--- TheCouponCode/TheCouponCode/Kata.cs
+++ TheCouponCode/TheCouponCode/Kata.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TheCouponCode
-{ 
+{
 public static class Kata
     {
         public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
