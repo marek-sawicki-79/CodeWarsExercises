d7b7cf961536692c395dc313e3ef623b1aea111a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-12 20:04:44 +0200

    Exercise prep

---
+++ GreetMe/GreetMe/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetMe
+{
+    public static class Kata
+    {
+        public static string Greet(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
