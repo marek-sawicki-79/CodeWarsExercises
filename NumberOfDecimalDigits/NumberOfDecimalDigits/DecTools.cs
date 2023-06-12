23b868430e5407cf8b75981e7795c5989ede9d1a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-12 20:14:52 +0200

    ExercisePrep

---
+++ NumberOfDecimalDigits/NumberOfDecimalDigits/DecTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfDecimalDigits
+{
+    public class DecTools
+    {
+        public static int Digits(ulong n)
+        {
+            return 0;
+        }
+    }
+}
