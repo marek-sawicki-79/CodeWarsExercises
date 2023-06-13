cbab5d06fd43e61d8fc76f21103fec6d9dc70b78
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-13 19:10:11 +0200

    exercise prep

---
+++ AllUnique/AllUnique/UnitTest1.cs
@@ -0,0 +1,29 @@
+namespace AllUnique
+{
+    public class UniqueTestClass
+    {
+        [Test]
+        public void TestHasUniqueChars1()
+        {
+            Assert.IsTrue(Unique.HasUniqueChars("abcdef"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars2()
+        {
+            Assert.False(Unique.HasUniqueChars("++-"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars3()
+        {
+            Assert.False(Unique.HasUniqueChars("  nAa"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars4()
+        {
+            Assert.False(Unique.HasUniqueChars("aba"));
+        }
+    }
+}
