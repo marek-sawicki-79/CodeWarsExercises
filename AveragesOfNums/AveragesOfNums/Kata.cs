3f6bf13ef4cafdb637f6b40be76c6bea8b9da30b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-14 19:01:59 +0200

    logic ok, need to handle null

--- AveragesOfNums/AveragesOfNums/Kata.cs
+++ AveragesOfNums/AveragesOfNums/Kata.cs
@@ -10,7 +10,13 @@ namespace AveragesOfNums
     {
         public static double[] Averages(int[] numbers)
         {
-            return new double[0];
+
+            double[] result = new double[numbers.Length - 1];
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                result[i - 1] = (numbers[i] + numbers[i - 1]) / 2.0;
+            }
+            return result;
         }
     }
 }
