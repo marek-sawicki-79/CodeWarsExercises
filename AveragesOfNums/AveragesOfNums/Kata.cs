f5f6a6a7039b5011110b24b6360a8e0c0b612a7b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-14 19:05:51 +0200

    done

--- AveragesOfNums/AveragesOfNums/Kata.cs
+++ AveragesOfNums/AveragesOfNums/Kata.cs
@@ -10,13 +10,16 @@ namespace AveragesOfNums
     {
         public static double[] Averages(int[] numbers)
         {
-
-            double[] result = new double[numbers.Length - 1];
-            for(int i = 1; i < numbers.Length; i++)
+            if(numbers == null || numbers.Length <= 1) return new double[0];
+            else
             {
-                result[i - 1] = (numbers[i] + numbers[i - 1]) / 2.0;
+                double[] result = new double[numbers.Length - 1];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    result[i - 1] = (numbers[i] + numbers[i - 1]) / 2.0;
+                }
+                return result;
             }
-            return result;
         }
     }
 }
