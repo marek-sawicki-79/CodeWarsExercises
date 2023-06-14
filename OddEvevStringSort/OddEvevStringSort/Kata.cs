cc722c7412dc34526ab3b3b83837962f6b85949a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-14 19:23:20 +0200

    cleaner with linq

--- OddEvevStringSort/OddEvevStringSort/Kata.cs
+++ OddEvevStringSort/OddEvevStringSort/Kata.cs
@@ -10,20 +10,21 @@ namespace OddEvevStringSort
     {
         public static string SortMyString(string s)
         {
-            List<char> evenChars = new List<char>();
-            List<char> oddChars = new List<char>();
-            for(int i = 0; i < s.Length; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    evenChars.Add(s[i]);
-                }
-                else
-                {
-                    oddChars.Add(s[i]);
-                }
-            }
-            return $"{string.Concat(evenChars)} {string.Concat(oddChars)}";
+            //List<char> evenChars = new List<char>();
+            //List<char> oddChars = new List<char>();
+            //for(int i = 0; i < s.Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        evenChars.Add(s[i]);
+            //    }
+            //    else
+            //    {
+            //        oddChars.Add(s[i]);
+            //    }
+            //}
+            //return $"{string.Concat(evenChars)} {string.Concat(oddChars)}";
+            return $"{string.Concat(s.Where((x, i) => i % 2 == 0))} {string.Concat(s.Where((x, i) => i % 2 != 0))}";
         }
-    }
+    }    
 }
