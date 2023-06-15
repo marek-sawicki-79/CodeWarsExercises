4bcdde3e4882c871185d01e80e621e92a6d33e5b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-15 19:46:04 +0200

    cleaner with Linq

--- FilterTheNumber/FilterTheNumber/Kata.cs
+++ FilterTheNumber/FilterTheNumber/Kata.cs
@@ -10,15 +10,16 @@ namespace FilterTheNumber
     {
         public static int FilterString(string s)
         {
-            string result = "";
-            for(int i = 0; i < s.Length; i++)
-            {
-                if (char.IsDigit(s[i]))
-                {
-                    result += s[i];
-                }
-            }
-            return int.Parse(result);
+            //string result = "";
+            //for(int i = 0; i < s.Length; i++)
+            //{
+            //    if (char.IsDigit(s[i]))
+            //    {
+            //        result += s[i];
+            //    }
+            //}
+            //return int.Parse(result);
+            return int.Parse(new String(s.Where(char.IsDigit).ToArray()));
         }
     }
 }
