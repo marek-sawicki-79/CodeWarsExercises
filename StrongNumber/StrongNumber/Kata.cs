92fed24492ac5ae97042639ff7297088060b92aa
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-15 19:36:07 +0200

    cleaner with linq

--- StrongNumber/StrongNumber/Kata.cs
+++ StrongNumber/StrongNumber/Kata.cs
@@ -8,9 +8,24 @@ namespace StrongNumber
 {
     class Kata
     {
+        private static int Factorial(int n) => n == 0 ? 1 : n * Factorial(n - 1);
         public static string StrongNumber(int number)
         {
-            //Do Some Magic
+            //string numbers = number.ToString();
+            //int factorialsSum = 0;
+            //foreach(char digit in numbers)
+            //{
+            //    int cyfra = int.Parse(digit.ToString());
+            //    int factorial = 1;
+            //    for(int i = 1; i <= cyfra; i++)
+            //    {
+            //        factorial *= i;
+            //    }
+            //    factorialsSum += factorial;
+            //}
+            //return (number == factorialsSum) ? "STRONG!!!!" : "Not Strong !!";
+            return number == number.ToString().Sum(n => Factorial(int.Parse(n.ToString())))
+            ? "STRONG!!!!" : "Not Strong !!";
         }
     }
 }
