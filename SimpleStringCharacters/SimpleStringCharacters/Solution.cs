57e63c95d9f7a394d391567d1556c378e7f6724e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-16 16:54:28 +0200

    Exercise prep

---
+++ SimpleStringCharacters/SimpleStringCharacters/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStringCharacters
+{
+    public class Solution
+    {
+        public static int[] solve(String s)
+        {
+            //..
+        }
+    }
+}
