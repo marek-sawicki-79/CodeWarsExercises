716b57ae9eea706934ae96e07f37b230d7db7717
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-16 17:14:08 +0200

    cleaner

--- SimpleStringCharacters/SimpleStringCharacters/Solution.cs
+++ SimpleStringCharacters/SimpleStringCharacters/Solution.cs
@@ -10,11 +10,18 @@ namespace SimpleStringCharacters
     {
         public static int[] solve(String s)
         {
-            int upperCase = s.Count(c => char.IsLetter(c) && char.IsUpper(c));
-            int lowerCase = s.Count(c => char.IsLetter(c) && char.IsLower(c));
-            int digits = s.Count(c => char.IsDigit(c));
-            int specialChars = s.Count(c => !char.IsDigit(c) && !char.IsLetter(c));
-            return new int[] { upperCase, lowerCase, digits, specialChars };
+            //int upperCase = s.Count(c => char.IsLetter(c) && char.IsUpper(c));
+            //int lowerCase = s.Count(c => char.IsLetter(c) && char.IsLower(c));
+            //int digits = s.Count(c => char.IsDigit(c));
+            //int specialChars = s.Count(c => !char.IsDigit(c) && !char.IsLetter(c));
+            //return new int[] { upperCase, lowerCase, digits, specialChars };
+            return new[]
+            {
+                s.Count(char.IsUpper),
+                s.Count(char.IsLower),
+                s.Count(char.IsDigit),
+                s.Count(x => !char.IsLetterOrDigit(x))
+            };
         }
     }
 }
