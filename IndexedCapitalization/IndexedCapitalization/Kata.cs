d7ea9d4ee6da64c6d12caa8dab035c26d6b5d8ef
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-17 22:17:30 +0200

    Much cleaner with Linq (from CodeWars)

--- IndexedCapitalization/IndexedCapitalization/Kata.cs
+++ IndexedCapitalization/IndexedCapitalization/Kata.cs
@@ -10,15 +10,16 @@ namespace IndexedCapitalization
     {
         public static string Capitalize(string s, List<int> idxs)
         {
-            char[] charArr = s.ToCharArray();
-            foreach(int index in idxs)
-            {
-                if(index >= 0 && index < charArr.Length)
-                {
-                    charArr[index] = char.ToUpper(charArr[index]);
-                }
-            }
-            return new string(charArr);
+            //char[] charArr = s.ToCharArray();
+            //foreach(int index in idxs)
+            //{
+            //    if(index >= 0 && index < charArr.Length)
+            //    {
+            //        charArr[index] = char.ToUpper(charArr[index]);
+            //    }
+            //}
+            //return new string(charArr);
+            return String.Concat(s.Select((c , i) => idxs.Contains(i) ? char.ToUpper(c) : c));  
         }
     }
 }
