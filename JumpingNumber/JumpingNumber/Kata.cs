2bbf756a59ddd09745ab7560807a4340e5799c06
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-18 21:24:21 +0200

    cleaner solution

--- JumpingNumber/JumpingNumber/Kata.cs
+++ JumpingNumber/JumpingNumber/Kata.cs
@@ -10,18 +10,19 @@ namespace JumpingNumber
     {
         public static string JumpingNumber(int number)
         {
-            string digits = number.ToString();
-            string result = "";
-            for(int i = 0; i < digits.Length - 1; i++)
-            {
-                if (Math.Abs(digits[i + 1] - digits[i]) != 1)
-                {
-                    result = "Not!!";
-                    break;
-                }
-            }
-            if (result != "Not!!") result = "Jumping!!";
-            return result;
+            //string digits = number.ToString();
+            //string result = "";
+            //for(int i = 0; i < digits.Length - 1; i++)
+            //{
+            //    if (Math.Abs(digits[i + 1] - digits[i]) != 1)
+            //    {
+            //        result = "Not!!";
+            //        break;
+            //    }
+            //}
+            //if (result != "Not!!") result = "Jumping!!";
+            //return result;
+            return number < 10 ? "Jumping!!" : Math.Abs(number % 10 - (number / 10) % 10) != 1 ? "Not!!" : JumpingNumber(number / 10);
         }
     }
 }
