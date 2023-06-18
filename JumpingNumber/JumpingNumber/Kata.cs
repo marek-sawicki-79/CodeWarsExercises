7a77d8ec7a126131ab54a264c88e718ae1485069
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-18 21:09:13 +0200

    Exercise prep

---
+++ JumpingNumber/JumpingNumber/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingNumber
+{
+    class Kata
+    {
+        public static string JumpingNumber(int number)
+        {
+            //Do Some Magic
+        }
+    }
+}
