8429517e08273fc0ceb3e671bc833afba20dbdfc
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-18 21:22:51 +0200

    done

--- JumpingNumber/JumpingNumber/Kata.cs
+++ JumpingNumber/JumpingNumber/Kata.cs
@@ -10,7 +10,18 @@ namespace JumpingNumber
     {
         public static string JumpingNumber(int number)
         {
-            //Do Some Magic
+            string digits = number.ToString();
+            string result = "";
+            for(int i = 0; i < digits.Length - 1; i++)
+            {
+                if (Math.Abs(digits[i + 1] - digits[i]) != 1)
+                {
+                    result = "Not!!";
+                    break;
+                }
+            }
+            if (result != "Not!!") result = "Jumping!!";
+            return result;
         }
     }
 }
