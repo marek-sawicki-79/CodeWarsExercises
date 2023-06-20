0eca35ffb34bcf12261538edcfdd93d85cfb55da
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-20 23:06:22 +0200

    linq

--- MultiplesOfN/MultiplesOfN/Kata.cs
+++ MultiplesOfN/MultiplesOfN/Kata.cs
@@ -10,12 +10,13 @@ namespace MultiplesOfN
     {
         public static double[] Multiples(int m, double n)
         {
-            double[] result = new double[m];
-            for (int i = 0; i < m; i++)
-            {
-                result[i] = n * (i + 1);
-            }
-            return result;
+            //double[] result = new double[m];
+            //for (int i = 0; i < m; i++)
+            //{
+            //    result[i] = n * (i + 1);
+            //}
+            //return result;
+            return Enumerable.Range(1, m).Select(x => x * n).ToArray();
         }
     }
 }
