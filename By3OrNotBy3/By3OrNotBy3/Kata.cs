2f056461b6fce6b4bc3e548487df66ff1d381aa2
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-23 18:06:14 +0200

    Exercise prep

---
+++ By3OrNotBy3/By3OrNotBy3/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace By3OrNotBy3
+{
+    public static class Kata
+    {
+        public static bool DivisibleByThree(string n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
