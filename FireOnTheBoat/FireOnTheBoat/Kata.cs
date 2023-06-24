a66f88d4ec948303368781cf406d5b20171c31c3
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-24 21:35:42 +0200

    easier, faster

--- FireOnTheBoat/FireOnTheBoat/Kata.cs
+++ FireOnTheBoat/FireOnTheBoat/Kata.cs
@@ -10,15 +10,17 @@ namespace FireOnTheBoat
     {
         public static string FireFight(string s)
         {
-            string[] boatItems = s.Split(" ");
-            for(int i = 0; i < boatItems.Length; i++)
-            {
-                if(boatItems[i] == "Fire")
-                {
-                    boatItems[i] = "~~";
-                }
-            }
-            return string.Join(" ", boatItems);
+            //string[] boatItems = s.Split(" ");
+            //for(int i = 0; i < boatItems.Length; i++)
+            //{
+            //    if(boatItems[i] == "Fire")
+            //    {
+            //        boatItems[i] = "~~";
+            //    }
+            //}
+            //return string.Join(" ", boatItems);
+
+            return s.Replace("Fire", "~~");
         }
     }
 }
