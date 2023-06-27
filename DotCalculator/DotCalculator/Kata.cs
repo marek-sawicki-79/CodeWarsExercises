ad07c4175a89861554abb7a28d29aa63a30be912
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-27 22:17:54 +0200

    done

--- DotCalculator/DotCalculator/Kata.cs
+++ DotCalculator/DotCalculator/Kata.cs
@@ -10,7 +10,30 @@ namespace DotCalculator
     {
         public static string Calculator(string txt)
         {
-
+            int dotCount = 0;
+            string result = "";
+            string[] elements = txt.Split(' ');
+            switch (elements[1])
+            {
+                case "+":
+                    dotCount = elements[0].Length + elements[2].Length;
+                    result = string.Join("", Enumerable.Repeat(".", dotCount));
+                    return result;
+                case "-":
+                    dotCount = elements[0].Length - elements[2].Length;
+                    result = string.Join("", Enumerable.Repeat(".", dotCount));
+                    return result;
+                case "//":
+                    dotCount = elements[0].Length / elements[2].Length;
+                    result = string.Join("", Enumerable.Repeat(".", dotCount));
+                    return result;
+                case "*":
+                    dotCount = elements[0].Length * elements[2].Length;
+                    result = string.Join("", Enumerable.Repeat(".", dotCount));
+                    return result;
+                    default:
+                    return "bad data";
+            }
         }
     }
 }
