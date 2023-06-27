f8bff6c150aee1f64c41451253221d2ae4566054
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-06-27 22:23:05 +0200

    interesting solution from CW using DataTable()

--- DotCalculator/DotCalculator/Kata.cs
+++ DotCalculator/DotCalculator/Kata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,30 +11,33 @@ namespace DotCalculator
     {
         public static string Calculator(string txt)
         {
-            int dotCount = 0;
-            string result = "";
-            string[] elements = txt.Split(' ');
-            switch (elements[1])
-            {
-                case "+":
-                    dotCount = elements[0].Length + elements[2].Length;
-                    result = string.Join("", Enumerable.Repeat(".", dotCount));
-                    return result;
-                case "-":
-                    dotCount = elements[0].Length - elements[2].Length;
-                    result = string.Join("", Enumerable.Repeat(".", dotCount));
-                    return result;
-                case "//":
-                    dotCount = elements[0].Length / elements[2].Length;
-                    result = string.Join("", Enumerable.Repeat(".", dotCount));
-                    return result;
-                case "*":
-                    dotCount = elements[0].Length * elements[2].Length;
-                    result = string.Join("", Enumerable.Repeat(".", dotCount));
-                    return result;
-                    default:
-                    return "bad data";
-            }
+            //int dotCount = 0;
+            //string result = "";
+            //string[] elements = txt.Split(' ');
+            //switch (elements[1])
+            //{
+            //    case "+":
+            //        dotCount = elements[0].Length + elements[2].Length;
+            //        result = string.Join("", Enumerable.Repeat(".", dotCount));
+            //        return result;
+            //    case "-":
+            //        dotCount = elements[0].Length - elements[2].Length;
+            //        result = string.Join("", Enumerable.Repeat(".", dotCount));
+            //        return result;
+            //    case "//":
+            //        dotCount = elements[0].Length / elements[2].Length;
+            //        result = string.Join("", Enumerable.Repeat(".", dotCount));
+            //        return result;
+            //    case "*":
+            //        dotCount = elements[0].Length * elements[2].Length;
+            //        result = string.Join("", Enumerable.Repeat(".", dotCount));
+            //        return result;
+            //        default:
+            //        return "bad data";
+            //}
+
+            var s = txt.Replace("//", "/").Split();
+            return new string('.', Convert.ToInt32(new DataTable().Compute($"{s[0].Length}{s[1]}{s[2].Length}", null)));
         }
     }
 }
