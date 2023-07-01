196986a5b6a5292f7be4aa409aa3f0cb0ef71a3d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-01 21:42:47 +0200

    exercise prep

---
+++ SpoonerizeMe/SpoonerizeMe/ReadySet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpoonerizeMe
+{
+    public static class ReadySet
+    {
+        public static string Spoonerize(string str)
+        {
+            return "";
+        }
+    }
+}
