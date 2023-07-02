ff91e49fd24ab669e4a0356ef92950ddb02ad37a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-02 19:16:14 +0200

    always cleaner with Linq

--- InitializeMyName/InitializeMyName/Kata.cs
+++ InitializeMyName/InitializeMyName/Kata.cs
@@ -10,15 +10,17 @@ namespace InitializeMyName
     {
         public static string InitializeNames(string name)
         {
-            string[] names = name.Split(' ');
-            if(names.Length > 2)
-            {
-                for(int i = 1; i < names.Length - 1; i++)
-                {
-                    names[i] = $"{names[i][0]}.";
-                }
-            }
-            return string.Join(" ", names);
+            //string[] names = name.Split(' ');
+            //if(names.Length > 2)
+            //{
+            //    for(int i = 1; i < names.Length - 1; i++)
+            //    {
+            //        names[i] = $"{names[i][0]}.";
+            //    }
+            //}
+            //return string.Join(" ", names);
+
+            return name.Split().Skip(1).SkipLast(1).Aggregate(name, (s, n) => s.Replace(n, n[0] + "."));
         }
     }
 }
