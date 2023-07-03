999411136ddcfc72f69a65e6ce9655cf6fce9057
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-03 20:06:20 +0200

    done

--- VeryEvenNumber/VeryEvenNumber/Kata.cs
+++ VeryEvenNumber/VeryEvenNumber/Kata.cs
@@ -10,17 +10,20 @@ namespace VeryEvenNumber
     {
         public static bool IsVeryEvenNumber(int number)
         {
-            int sum = number;
-            while(number.ToString().Count() > 1)
+            if (number < 10)
             {
-                char[] nums = number.ToString().ToCharArray();
-                foreach(char c in nums)
-                {
-                    sum += int.Parse(c.ToString());
-                }
-                number = sum;
+                return number % 2 == 0;
             }
-            return (number % 2 == 0) ? true : false;
+            else
+            {
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                return IsVeryEvenNumber(sum);
+            }
         }
     }
 }
