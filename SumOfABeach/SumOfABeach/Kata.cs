70461c2096bb737b467a4df7460f34f94e0a8b8a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-06 13:29:56 +0200

    faster with regex

--- SumOfABeach/SumOfABeach/Kata.cs
+++ SumOfABeach/SumOfABeach/Kata.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SumOfABeach
@@ -10,14 +11,16 @@ namespace SumOfABeach
     {
         public static int SumOfABeach(string s)
         {
-            string[] beachParty = { "sand", "water", "fish", "sun" };
-            int counter = 0;
-            foreach (string party in beachParty)
-            {
-                int index = -1;
-                while ((index = s.ToLower().IndexOf(party, index + 1)) != -1) counter++;
-            }
-            return counter;
+            //string[] beachParty = { "sand", "water", "fish", "sun" };
+            //int counter = 0;
+            //foreach (string party in beachParty)
+            //{
+            //    int index = -1;
+            //    while ((index = s.ToLower().IndexOf(party, index + 1)) != -1) counter++;
+            //}
+            //return counter;
+
+            return Regex.Matches(s.ToLower(), "water|fish|sand|sun").Count;
         }
     }
 }
