b5453794c3ae94b11deb06e1020f552a4461c046
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-07 20:18:12 +0200

    do not handle case when radius is not number

--- AreaOfACircle/AreaOfACircle/Kata.cs
+++ AreaOfACircle/AreaOfACircle/Kata.cs
@@ -10,7 +10,16 @@ namespace AreaOfACircle
     {
         public static double CalculateAreaOfCircle(string radius)
         {
-            //Solve!
+            double rad = double.Parse(radius);
+            double pi = 3.14;
+            if(rad  <= 0.0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return rad * rad * pi;
+            }
         }
     }
 }
