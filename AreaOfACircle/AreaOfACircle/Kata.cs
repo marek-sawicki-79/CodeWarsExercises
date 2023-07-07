a8f88ae75f8abfb4578074a0ca7df36e09436446
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-07 20:07:46 +0200

    exercise prep

---
+++ AreaOfACircle/AreaOfACircle/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfACircle
+{
+    public static class Kata
+    {
+        public static double CalculateAreaOfCircle(string radius)
+        {
+            //Solve!
+        }
+    }
+}
