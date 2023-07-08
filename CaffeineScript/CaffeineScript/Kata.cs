2d29985166993352701ae3dbdb6bcf48143f04a7
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-08 16:37:27 +0200

    more efficient oneliner from CW

--- CaffeineScript/CaffeineScript/Kata.cs
+++ CaffeineScript/CaffeineScript/Kata.cs
@@ -10,28 +10,30 @@ namespace CaffeineScript
     {
         public static string CaffeineBuzz(int n)
         {
-            string result = "";
-            if(n % 3 == 0 && n % 4 == 0)
-            {
-                result = "Coffee";
-                if(n % 2 ==0)
-                {
-                    result = $"{result}Script";
-                }
-            }
-            else if (n % 3 == 0)
-            {
-                result = "Java";
-                if (n % 2 == 0)
-                {
-                    result = $"{result}Script";
-                }
-            }
-            else
-            {
-                result = "mocha_missing!";
-            }
-            return result;
+            //string result = "";
+            //if(n % 3 == 0 && n % 4 == 0)
+            //{
+            //    result = "Coffee";
+            //    if(n % 2 ==0)
+            //    {
+            //        result = $"{result}Script";
+            //    }
+            //}
+            //else if (n % 3 == 0)
+            //{
+            //    result = "Java";
+            //    if (n % 2 == 0)
+            //    {
+            //        result = $"{result}Script";
+            //    }
+            //}
+            //else
+            //{
+            //    result = "mocha_missing!";
+            //}
+            //return result;
+
+            return n % 3 == 0 ? (n % 4 == 0 ? "Coffee" : "Java") + (n % 2 == 0 ? "Script" : "") : "mocha_missing!";
         }
     }
 }
