3af55c4cfe8b59a33e7d1c95d7dfcecff0c8b557
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-09 17:29:48 +0200

    exercise prep

---
+++ CharacterCounter/CharacterCounter/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCounter
+{
+    public class Kata
+    {
+        public static bool ValidateWord(string s)
+        {
+            //Code goes here!
+        }
+    }
+}
