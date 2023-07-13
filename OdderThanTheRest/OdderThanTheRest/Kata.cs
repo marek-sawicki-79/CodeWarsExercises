799aee0a0184e20205535990a4b79d95f6040d56
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-13 20:49:23 +0200

    done

--- OdderThanTheRest/OdderThanTheRest/Kata.cs
+++ OdderThanTheRest/OdderThanTheRest/Kata.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace OdderThanTheRest
 {
-    internal class Kata
+    public class Kata
     {
+        public static int OddOne(List<int> list)
+        {
+            int result = 0;
+            foreach(int i in list)
+            {
+                if (i % 2 != 0)
+                {
+                    result = list.IndexOf(i);
+                    break;
+                }
+                else result = -1;
+            }
+            return result;
+        }
     }
 }
--- OdderThanTheRest/OdderThanTheRest/UnitTest1.cs
+++ OdderThanTheRest/OdderThanTheRest/UnitTest1.cs
@@ -1,16 +1,15 @@
 namespace OdderThanTheRest
 {
-    public class Tests
+    [TestFixture]
+    public class SolutionTest
     {
-        [SetUp]
-        public void Setup()
+        [Test, Description("Your solution should pass basic tests")]
+        public void FixedTest()
         {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+            Assert.AreEqual(3, Kata.OddOne(new List<int> { 2, 4, 6, 7, 10 }));
+            Assert.AreEqual(4, Kata.OddOne(new List<int> { 2, 16, 98, 10, 13, 78 }));
+            Assert.AreEqual(4, Kata.OddOne(new List<int> { 4, -8, 98, -12, -7, 90, 100 }));
+            Assert.AreEqual(-1, Kata.OddOne(new List<int> { 2, 4, 6, 8 }));
         }
     }
 }
