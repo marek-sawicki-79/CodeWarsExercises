0d6fe66411d77f9bff740d3ff57c43d2df28aa9f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-13 20:41:25 +0200

    exercise prep

---
+++ OdderThanTheRest/OdderThanTheRest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OdderThanTheRest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
