713e544490ed57be00049c6843f214e5ec08406e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-16 19:14:50 +0200

    exercise prep

---
+++ WordValues/WordValues/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WordValues
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
