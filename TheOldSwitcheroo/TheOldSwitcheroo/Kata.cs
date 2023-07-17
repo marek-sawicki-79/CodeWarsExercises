3c960d09c15b89ad3592c071fb343d47222fd5e2
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-17 21:30:24 +0200

    one liner with regex (CW)

--- TheOldSwitcheroo/TheOldSwitcheroo/Kata.cs
+++ TheOldSwitcheroo/TheOldSwitcheroo/Kata.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TheOldSwitcheroo
@@ -11,18 +12,20 @@ namespace TheOldSwitcheroo
     {
         public static string Vowel2Index(string str)
         {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            StringBuilder newStr = new StringBuilder();
-            for(int i = 0; i < str.Length; i++)
-            {
-                char c = str[i];
-                if (vowels.Contains(char.ToLower(c)))
-                {
-                    newStr.Append((i + 1).ToString());
-                }
-                else newStr.Append(c);
-            }
-            return newStr.ToString();
+            //char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            //StringBuilder newStr = new StringBuilder();
+            //for(int i = 0; i < str.Length; i++)
+            //{
+            //    char c = str[i];
+            //    if (vowels.Contains(char.ToLower(c)))
+            //    {
+            //        newStr.Append((i + 1).ToString());
+            //    }
+            //    else newStr.Append(c);
+            //}
+            //return newStr.ToString();
+
+            return Regex.Replace(str, "[aeiou]", x => (x.Index + 1).ToString());
         }
     }
 }
