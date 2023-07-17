ccd639787017a51ce60ede8b9acd8c14f2dea5bc
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-17 21:29:20 +0200

    done

--- TheOldSwitcheroo/TheOldSwitcheroo/Kata.cs
+++ TheOldSwitcheroo/TheOldSwitcheroo/Kata.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheOldSwitcheroo
 {
-    internal class Kata
+    public class Kata
     {
+        public static string Vowel2Index(string str)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            StringBuilder newStr = new StringBuilder();
+            for(int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if (vowels.Contains(char.ToLower(c)))
+                {
+                    newStr.Append((i + 1).ToString());
+                }
+                else newStr.Append(c);
+            }
+            return newStr.ToString();
+        }
     }
 }
--- TheOldSwitcheroo/TheOldSwitcheroo/UnitTest1.cs
+++ TheOldSwitcheroo/TheOldSwitcheroo/UnitTest1.cs
@@ -1,16 +1,16 @@
 namespace TheOldSwitcheroo
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        [TestCase("this is my string", ExpectedResult = "th3s 6s my str15ng")]
+        [TestCase("Codewars is the best site in the world", ExpectedResult = "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld")]
+        [TestCase("Tomorrow is going to be raining", ExpectedResult = "T2m4rr7w 10s g1415ng t20 b23 r2627n29ng")]
+        [TestCase("", ExpectedResult = "")]
+        public static string FixedTest(string str)
         {
-            Assert.Pass();
+            return Kata.Vowel2Index(str);
         }
     }
 }
