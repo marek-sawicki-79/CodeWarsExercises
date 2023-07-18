1cc149fbaa2d5811aff73f92d2c44e58a898c955
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-18 18:15:35 +0200

    shorter with replace method

--- Switcheroo/Switcheroo/Kata.cs
+++ Switcheroo/Switcheroo/Kata.cs
@@ -10,12 +10,14 @@ namespace Switcheroo
     {
         public static string Switcheroo(string x)
         {
-            StringBuilder newX = new StringBuilder();
-            foreach (char c in x)
-            {
-                _ = (c == 'a') ? newX.Append('b') : (c == 'b') ? newX.Append('a') : newX.Append(c);
-            }
-            return newX.ToString();
+            //StringBuilder newX = new StringBuilder();
+            //foreach (char c in x)
+            //{
+            //    _ = (c == 'a') ? newX.Append('b') : (c == 'b') ? newX.Append('a') : newX.Append(c);
+            //}
+            //return newX.ToString();
+
+            return x.Replace("a", "d").Replace("b", "a").Replace("d", "b");
         }
     }
 }
