cabff61f454e438975da3d89c78ae2fb628457f4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-20 14:15:54 +0200

    done

--- ReturnMissingElement/ReturnMissingElement/Kata.cs
+++ ReturnMissingElement/ReturnMissingElement/Kata.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ReturnMissingElement
 {
-    internal class Kata
+    public static class Kata
     {
+        public static int GetMissingElement(int[] superImportantArray)
+        {
+            int[] blabla = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            return blabla.Except(superImportantArray).Single();
+        }
     }
 }
--- ReturnMissingElement/ReturnMissingElement/UnitTest1.cs
+++ ReturnMissingElement/ReturnMissingElement/UnitTest1.cs
@@ -1,16 +1,18 @@
 namespace ReturnMissingElement
 {
-    public class Tests
+    [TestFixture]
+    public class MissingElement
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void EightIsMissing()
         {
+            Assert.AreEqual(8, Kata.GetMissingElement(new int[9] { 0, 5, 1, 3, 2, 9, 7, 6, 4 }));
         }
 
         [Test]
-        public void Test1()
+        public void ThreeIsMissing()
         {
-            Assert.Pass();
+            Assert.AreEqual(3, Kata.GetMissingElement(new int[9] { 9, 2, 4, 5, 7, 0, 8, 6, 1 }));
         }
     }
 }
