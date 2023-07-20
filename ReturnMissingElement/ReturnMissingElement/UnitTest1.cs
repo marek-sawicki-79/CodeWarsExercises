1893fa5ba34281928c07a0011a0408b8dcfad218
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-20 14:04:16 +0200

    Exercise prep

---
+++ ReturnMissingElement/ReturnMissingElement/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ReturnMissingElement
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
