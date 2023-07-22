31efc22812b5bca0a181a2cf35cf416b34757a61
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-22 12:10:56 +0200

    cleaner one liner

--- LastDigitsOfANumber/LastDigitsOfANumber/Kata.cs
+++ LastDigitsOfANumber/LastDigitsOfANumber/Kata.cs
@@ -10,24 +10,26 @@ namespace LastDigitsOfANumber
     {
         public static int[] LastDigit(long n, int d)
         {
-            List<int> result = new List<int>();
-            if (d <= 0)
-            {
-                return result.ToArray();
-            }
-            else
-            {
-                string nToString = n.ToString();
-                if (d > nToString.Length)
-                {
-                    return (n.ToString()).Select(x => int.Parse(x.ToString())).ToArray();
-                }
-                else
-                {
-                    result = nToString.Substring(nToString.Length - d).Select(x => int.Parse(x.ToString())).ToList();
-                }
-            }
-            return result.ToArray();
+            //List<int> result = new List<int>();
+            //if (d <= 0)
+            //{
+            //    return result.ToArray();
+            //}
+            //else
+            //{
+            //    string nToString = n.ToString();
+            //    if (d > nToString.Length)
+            //    {
+            //        return (n.ToString()).Select(x => int.Parse(x.ToString())).ToArray();
+            //    }
+            //    else
+            //    {
+            //        result = nToString.Substring(nToString.Length - d).Select(x => int.Parse(x.ToString())).ToList();
+            //    }
+            //}
+            //return result.ToArray();
+
+            return $"{n}".TakeLast(d).Select(x => x - '0').ToArray();
         }
     }
 }
