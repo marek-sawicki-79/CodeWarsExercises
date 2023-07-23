4dbd6fd98467fd33e8d2fc5c58a926e4fbef329b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-23 13:47:51 +0200

    Exercise prep

---
+++ COuntConsonants/COuntConsonants/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace COuntConsonants
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
