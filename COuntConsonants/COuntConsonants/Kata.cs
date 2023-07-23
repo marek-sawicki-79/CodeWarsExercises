9f12abe8e9f63d603592584857f13aa4a0d89584
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-23 14:07:35 +0200

    cleaner solution with Regex

--- COuntConsonants/COuntConsonants/Kata.cs
+++ COuntConsonants/COuntConsonants/Kata.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace COuntConsonants
@@ -10,9 +11,11 @@ namespace COuntConsonants
     {
         public static int ConsonantCount(string str)
         {
-            char[] vowels = { 'o', 'i', 'u', 'e', 'a' };
-            string halfResult = new string(str.ToLower().Where(Char.IsLetter).ToArray());
-            return halfResult.ToCharArray().Count(c => !vowels.Contains(c));
+            //char[] vowels = { 'o', 'i', 'u', 'e', 'a' };
+            //string halfResult = new string(str.ToLower().Where(Char.IsLetter).ToArray());
+            //return halfResult.ToCharArray().Count(c => !vowels.Contains(c));
+
+            return Regex.Matches(str.ToLower(), "[b-df-hj-np-tv-z]").Count;
         }
     }
 }
