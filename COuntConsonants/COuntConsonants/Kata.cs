dfae10ffe750db33c53a84888b12c5b8f1c18bef
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-23 14:08:57 +0200

    refactor of 1st solution

--- COuntConsonants/COuntConsonants/Kata.cs
+++ COuntConsonants/COuntConsonants/Kata.cs
@@ -15,7 +15,11 @@ namespace COuntConsonants
             //string halfResult = new string(str.ToLower().Where(Char.IsLetter).ToArray());
             //return halfResult.ToCharArray().Count(c => !vowels.Contains(c));
 
-            return Regex.Matches(str.ToLower(), "[b-df-hj-np-tv-z]").Count;
+            //return Regex.Matches(str.ToLower(), "[b-df-hj-np-tv-z]").Count;
+
+            return str.ToLower()
+          .Where(x => Char.IsLetter(x) && !"aeiou".Contains(x))
+          .Count();
         }
     }
 }
