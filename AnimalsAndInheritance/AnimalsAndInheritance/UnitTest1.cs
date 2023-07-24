0a2eb661fc281b34c0196ea4b1842a2ea7da1195
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-24 16:33:15 +0200

    Exercise prep

---
+++ AnimalsAndInheritance/AnimalsAndInheritance/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AnimalsAndInheritance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
