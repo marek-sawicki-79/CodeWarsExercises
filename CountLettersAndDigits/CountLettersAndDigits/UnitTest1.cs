d51bd31aea7d86a876426858b4fcdd1174131372
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-25 14:32:59 +0200

    Exercise prep

---
+++ CountLettersAndDigits/CountLettersAndDigits/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CountLettersAndDigits
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
