d2e4a4bd7449dda6a84ea18d5bf03350f619546e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-25 14:39:21 +0200

    Done. Again

--- CountLettersAndDigits/CountLettersAndDigits/Kata.cs
+++ CountLettersAndDigits/CountLettersAndDigits/Kata.cs
@@ -10,7 +10,8 @@ namespace CountLettersAndDigits
     {
         public static int CountLettersAndDigits(string input)
         {
-            return input.Where(char.IsLetterOrDigit).Count();
+            //return input.Where(char.IsLetterOrDigit).Count();
+            return input.Count(x => char.IsLetterOrDigit(x));
         }
     }
 }
