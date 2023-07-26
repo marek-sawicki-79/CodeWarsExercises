fa625805331a2bcc2f51ba7f4b83655a2479df1d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-26 16:54:04 +0200

    exercise prep

---
+++ PreviousMultipleOf3/PreviousMultipleOf3/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreviousMultipleOf3
+{
+    internal class Kata
+    {
+    }
+}
---
+++ PreviousMultipleOf3/PreviousMultipleOf3/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PreviousMultipleOf3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
