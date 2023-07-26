01f5c73128ef47393021d128f022a731e3c9c9b9
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-26 17:02:12 +0200

    done

--- PreviousMultipleOf3/PreviousMultipleOf3/Kata.cs
+++ PreviousMultipleOf3/PreviousMultipleOf3/Kata.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace PreviousMultipleOf3
 {
-    internal class Kata
+    public static class Kata
     {
+        public static int? PreviousMultipleOfThree(int n)
+        {
+            int? result = null;
+            while (n.ToString().Length > 0)
+            {
+                if (n % 3 == 0)
+                {
+                    result = n;
+                    break;
+                }
+                n = n / 10;
+            }
+            if (result == 0)
+            {
+                result = null;
+            }
+            return result;
+        }
     }
 }
--- PreviousMultipleOf3/PreviousMultipleOf3/UnitTest1.cs
+++ PreviousMultipleOf3/PreviousMultipleOf3/UnitTest1.cs
@@ -1,16 +1,23 @@
 namespace PreviousMultipleOf3
 {
-    public class Tests
+    [TestFixture]
+    public class SolutionTest
     {
-        [SetUp]
-        public void Setup()
+        private static void Act(int n, int? expected)
         {
+            var msg = $"Invalid answer for n: {n}";
+            var actual = Kata.PreviousMultipleOfThree(n);
+            Assert.AreEqual(expected, actual, msg);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(1, null)]
+        [TestCase(25, null)]
+        [TestCase(36, 36)]
+        [TestCase(1244, 12)]
+        [TestCase(952406, 9)]
+        public void SampleTests(int n, int? expected)
         {
-            Assert.Pass();
+            Act(n, expected);
         }
     }
 }
