7b8eaa6f3c514e02f37d8d379c5bc24e4e182aa5
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-27 23:24:51 +0200

    done

--- AsciiStringSumCompare/AsciiStringSumCompare/Kata.cs
+++ AsciiStringSumCompare/AsciiStringSumCompare/Kata.cs
@@ -10,63 +10,93 @@ namespace AsciiStringSumCompare
     {
         public static bool Compare(string s1, string s2)
         {
-            //char[] s1Chars = s1.ToCharArray();
-            //char[] s2Chars = s2.ToCharArray();
-            //int sumS1 = 0;
-            //int sumS2 = 0;
-            //for (int i = 0; i < s1Chars.Length; i++)
+            ////char[] s1Chars = s1.ToCharArray();
+            ////char[] s2Chars = s2.ToCharArray();
+            ////int sumS1 = 0;
+            ////int sumS2 = 0;
+            ////for (int i = 0; i < s1Chars.Length; i++)
+            ////{
+            ////    if (!char.IsLetter(s1Chars[i]))
+            ////    {
+            ////        sumS1 = 0;
+            ////    }
+            ////    else
+            ////    {
+            ////        sumS1 += Convert.ToInt32(s1Chars[i]);
+            ////    }
+            ////}
+            ////for (int j = 0; j < s2Chars.Length; j++)
+            ////{
+            ////    if (!char.IsLetter(s2Chars[j]))
+            ////    {
+            ////        sumS2 = 0;
+            ////    }
+            ////    else
+            ////    {
+            ////        sumS2 += Convert.ToInt32(s2Chars[j]);
+            ////    }
+            ////}
+            ////return sumS1 == sumS2;
+            //if (s1 == null) s1 = "";
+            //if (s2 == null) s2 = "";
+
+            //s1 = s1.ToUpper();
+            //s2 = s2.ToUpper();
+
+            //int sum1 = 0;
+            //int sum2 = 0;
+
+            //foreach (char c in s1)
             //{
-            //    if (!char.IsLetter(s1Chars[i]))
+            //    if (char.IsDigit(c))
             //    {
-            //        sumS1 = 0;
+            //        s1 = "";
             //    }
-            //    else
+            //    else if(char.IsLetter(c))
             //    {
-            //        sumS1 += Convert.ToInt32(s1Chars[i]);
+            //        sum1 += (int)c;
             //    }
             //}
-            //for (int j = 0; j < s2Chars.Length; j++)
+            //foreach (char c in s2)
             //{
-            //    if (!char.IsLetter(s2Chars[j]))
+            //    if (char.IsDigit(c))
             //    {
-            //        sumS2 = 0;
+            //        s2 = "";
             //    }
-            //    else
+            //    else if (char.IsLetter(c))
             //    {
-            //        sumS2 += Convert.ToInt32(s2Chars[j]);
+            //        sum2 += (int)c;
             //    }
             //}
-            //return sumS1 == sumS2;
+
+            //return sum1 == sum2;
+
             if (s1 == null) s1 = "";
             if (s2 == null) s2 = "";
 
             s1 = s1.ToUpper();
             s2 = s2.ToUpper();
 
+            if (s1.Any(c => char.IsDigit(c)) || s2.Any(c => char.IsDigit(c)))
+                return true;
+
             int sum1 = 0;
             int sum2 = 0;
 
             foreach (char c in s1)
             {
-                if (char.IsDigit(c))
-                {
-                    s1 = "";
-                }
-                else if(char.IsLetter(c))
-                {
+                if (char.IsLetter(c))
                     sum1 += (int)c;
-                }
+                else
+                    return false;
             }
+
             foreach (char c in s2)
             {
-                if (char.IsDigit(c))
-                {
-                    s2 = "";
-                }
-                else if (char.IsLetter(c))
-                {
+                if (char.IsLetter(c))
                     sum2 += (int)c;
-                }
+                else
+                    return false;
             }
 
             return sum1 == sum2;
