0f042d1ee97d5cfe1a12d499f1a9c55b4d24c60f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-27 23:26:31 +0200

    a bit cleaner and more elegant solution from CW

--- AsciiStringSumCompare/AsciiStringSumCompare/Kata.cs
+++ AsciiStringSumCompare/AsciiStringSumCompare/Kata.cs
@@ -71,35 +71,53 @@ namespace AsciiStringSumCompare
 
             //return sum1 == sum2;
 
-            if (s1 == null) s1 = "";
-            if (s2 == null) s2 = "";
+            //////if (s1 == null) s1 = "";
+            //////if (s2 == null) s2 = "";
 
-            s1 = s1.ToUpper();
-            s2 = s2.ToUpper();
+            //////s1 = s1.ToUpper();
+            //////s2 = s2.ToUpper();
 
-            if (s1.Any(c => char.IsDigit(c)) || s2.Any(c => char.IsDigit(c)))
-                return true;
+            //////if (s1.Any(c => char.IsDigit(c)) || s2.Any(c => char.IsDigit(c)))
+            //////    return true;
 
-            int sum1 = 0;
-            int sum2 = 0;
+            //////int sum1 = 0;
+            //////int sum2 = 0;
 
-            foreach (char c in s1)
+            //////foreach (char c in s1)
+            //////{
+            //////    if (char.IsLetter(c))
+            //////        sum1 += (int)c;
+            //////    else
+            //////        return false;
+            //////}
+
+            //////foreach (char c in s2)
+            //////{
+            //////    if (char.IsLetter(c))
+            //////        sum2 += (int)c;
+            //////    else
+            //////        return false;
+            //////}
+
+            //////return sum1 == sum2;
+
+            if (s1 == null)
             {
-                if (char.IsLetter(c))
-                    sum1 += (int)c;
-                else
-                    return false;
+                s1 = "";
             }
-
-            foreach (char c in s2)
+            if (s2 == null)
             {
-                if (char.IsLetter(c))
-                    sum2 += (int)c;
-                else
-                    return false;
+                s2 = "";
             }
-
-            return sum1 == sum2;
+            if (!s1.All(o => char.IsLetter(o)))
+            {
+                s1 = "";
+            }
+            if (!s2.All(o => char.IsLetter(o)))
+            {
+                s2 = "";
+            }
+            return s1.ToUpper().Select(o => (int)o).Sum() == s2.ToUpper().Select(o => (int)o).Sum();
         }
     }
 }
