5b5ea785484070454d71b5c3f65b7160401bb8bc
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-27 23:32:09 +0200

    cleaner solution

--- AsciiStringSumCompare/AsciiStringSumCompare/Kata.cs
+++ AsciiStringSumCompare/AsciiStringSumCompare/Kata.cs
@@ -10,57 +10,9 @@ namespace AsciiStringSumCompare
     {
         public static bool Compare(string s1, string s2)
         {
-
-
-            //return sum1 == sum2;
-
-            //////if (s1 == null) s1 = "";
-            //////if (s2 == null) s2 = "";
-
-            //////s1 = s1.ToUpper();
-            //////s2 = s2.ToUpper();
-
-            //////if (s1.Any(c => char.IsDigit(c)) || s2.Any(c => char.IsDigit(c)))
-            //////    return true;
-
-            //////int sum1 = 0;
-            //////int sum2 = 0;
-
-            //////foreach (char c in s1)
-            //////{
-            //////    if (char.IsLetter(c))
-            //////        sum1 += (int)c;
-            //////    else
-            //////        return false;
-            //////}
-
-            //////foreach (char c in s2)
-            //////{
-            //////    if (char.IsLetter(c))
-            //////        sum2 += (int)c;
-            //////    else
-            //////        return false;
-            //////}
-
-            //////return sum1 == sum2;
-
-            if (s1 == null)
-            {
-                s1 = "";
-            }
-            if (s2 == null)
-            {
-                s2 = "";
-            }
-            if (!s1.All(o => char.IsLetter(o)))
-            {
-                s1 = "";
-            }
-            if (!s2.All(o => char.IsLetter(o)))
-            {
-                s2 = "";
-            }
-            return s1.ToUpper().Select(o => (int)o).Sum() == s2.ToUpper().Select(o => (int)o).Sum();
+            if (String.IsNullOrEmpty(s1) || String.IsNullOrEmpty(s2) || (s1 + s2).Any(c => !char.IsLetter(c)))
+                return true;
+            return s1.ToUpper().Sum(c =>c) == s2.ToUpper().Sum(c => c);
         }
     }
 }
