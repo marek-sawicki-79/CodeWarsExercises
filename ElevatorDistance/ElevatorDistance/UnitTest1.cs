f7d3efafd9a29068a90e2321c4d8ddb74427298e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-28 23:36:07 +0200

    Exercise prep

---
+++ ElevatorDistance/ElevatorDistance/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ElevatorDistance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
