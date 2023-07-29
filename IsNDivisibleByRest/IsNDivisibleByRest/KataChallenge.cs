da0488c6f064ca75bf1c3884c67c32d64931aba0
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-29 21:39:32 +0200

    done

--- IsNDivisibleByRest/IsNDivisibleByRest/KataChallenge.cs
+++ IsNDivisibleByRest/IsNDivisibleByRest/KataChallenge.cs
@@ -8,9 +8,18 @@ namespace IsNDivisibleByRest
 {
     public static class KataChallenge
     {
-        public static bool IsDivisible(int a, int b, int c)
+        public static bool IsDivisible(params int[] numbers)
         {
-            return (a % b == 0 && a % c == 0) ? true : false;
+           if (numbers.Length < 2) return false;
+            else
+            {
+                int firstNum = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (firstNum % numbers[i] != 0) return false;
+                }
+                return true;
+            }
         }
     }
 }
