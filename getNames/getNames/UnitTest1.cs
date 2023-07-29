f638d6d9c9a3405f7625860396e272e620d3fb6d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-29 20:51:18 +0200

    exercise prep

---
+++ getNames/getNames/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace getNames
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
