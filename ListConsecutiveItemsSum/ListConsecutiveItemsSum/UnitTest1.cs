3d99dfa2322d590a242e75f99461de5a66f65b3a
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-30 22:52:47 +0200

    exercise prep

---
+++ ListConsecutiveItemsSum/ListConsecutiveItemsSum/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ListConsecutiveItemsSum
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
