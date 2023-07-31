2053e1c1374f86a6991181b41c759406edc2617d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-31 17:50:05 +0200

    done

--- YearDifference/YearDifference/Kata.cs
+++ YearDifference/YearDifference/Kata.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace YearDifference
 {
-    internal class Kata
+    public static class Kata
     {
+        public static int HowManyYears(string date1, string date2)
+        {
+            return Math.Abs(int.Parse(date1.Substring(0, 4)) - int.Parse(date2.Substring(0, 4)));
+        }
     }
 }
--- YearDifference/YearDifference/UnitTest1.cs
+++ YearDifference/YearDifference/UnitTest1.cs
@@ -1,16 +1,17 @@
 namespace YearDifference
 {
-    public class Tests
+    [TestFixture]
+    public class KataTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void SampleTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(18, Kata.HowManyYears("1997/10/10", "2015/10/10"));
+            Assert.AreEqual(25, Kata.HowManyYears("1990/10/10", "2015/10/10"));
+            Assert.AreEqual(25, Kata.HowManyYears("2015/10/10", "1990/10/10"));
+            Assert.AreEqual(23, Kata.HowManyYears("1992/10/24", "2015/10/24"));
+            Assert.AreEqual(18, Kata.HowManyYears("2018/10/10", "2000/10/10"));
+            Assert.AreEqual(0, Kata.HowManyYears("2000/10/10", "2000/10/10"));
         }
     }
 }
