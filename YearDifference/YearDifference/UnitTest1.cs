09732418dc31f3275b55772984de950114810265
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-07-31 17:46:28 +0200

    exercise prep

---
+++ YearDifference/YearDifference/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace YearDifference
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
