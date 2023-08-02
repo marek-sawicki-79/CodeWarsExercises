f2c8d4eb57283a9489ffba5fd1e65441520cb966
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-02 20:56:43 +0200

    oneliner

--- GeometricProgressionSequence/GeometricProgressionSequence/Kata.cs
+++ GeometricProgressionSequence/GeometricProgressionSequence/Kata.cs
@@ -10,12 +10,14 @@ namespace GeometricProgressionSequence
     {
         public static string GeometricSequenceElements(int a, int r, int n)
         {
-            int[] geo = new int[n];
-            for (int i = 0;  i < n; i++)
-            {
-                geo[i] = a * (int)Math.Pow(r, i);
-            }
-            return string.Join(", ", geo);
+            //int[] geo = new int[n];
+            //for (int i = 0;  i < n; i++)
+            //{
+            //    geo[i] = a * (int)Math.Pow(r, i);
+            //}
+            //return string.Join(", ", geo);
+
+            return string.Join(", ", Enumerable.Range(0, n).Select(i => Math.Pow(r, i) * a));
         }
     }
 }
