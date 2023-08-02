c8e7d75f61edeec592b82feae92228482ea26806
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-02 20:53:00 +0200

    done

--- GeometricProgressionSequence/GeometricProgressionSequence/Kata.cs
+++ GeometricProgressionSequence/GeometricProgressionSequence/Kata.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace GeometricProgressionSequence
 {
-    internal class Kata
+    public class Kata
     {
+        public static string GeometricSequenceElements(int a, int r, int n)
+        {
+            int[] geo = new int[n];
+            for (int i = 0;  i < n; i++)
+            {
+                geo[i] = a * (int)Math.Pow(r, i);
+            }
+            return string.Join(", ", geo);
+        }
     }
 }
--- GeometricProgressionSequence/GeometricProgressionSequence/UnitTest1.cs
+++ GeometricProgressionSequence/GeometricProgressionSequence/UnitTest1.cs
@@ -1,16 +1,14 @@
 namespace GeometricProgressionSequence
 {
-    public class Tests
+    [TestFixture]
+    public class GeometricSequenceElementsTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void FixedTests()
         {
-            Assert.Pass();
+            Assert.AreEqual("2, 6, 18, 54, 162", Kata.GeometricSequenceElements(2, 3, 5));
+            Assert.AreEqual("2, 4, 8, 16, 32, 64, 128, 256, 512, 1024", Kata.GeometricSequenceElements(2, 2, 10));
+            Assert.AreEqual("1, -2, 4, -8, 16, -32, 64, -128, 256, -512", Kata.GeometricSequenceElements(1, -2, 10));
         }
     }
 }
