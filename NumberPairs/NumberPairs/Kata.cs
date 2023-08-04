0fe16e162b6878fcc264c88f0b372a6f3fba3a3b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-04 21:55:52 +0200

    done

--- NumberPairs/NumberPairs/Kata.cs
+++ NumberPairs/NumberPairs/Kata.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace NumberPairs
 {
-    internal class Kata
+    public class Kata
     {
+        public static int[] getLargerNumbers(int[] a, int[] b)
+        {
+            int[] result = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                _ = (a[i] > b[i]) ? result[i] = a[i] : result[i] = b[i];
+            }
+            return result;
+        }
     }
 }
--- NumberPairs/NumberPairs/UnitTest1.cs
+++ NumberPairs/NumberPairs/UnitTest1.cs
@@ -1,16 +1,14 @@
 namespace NumberPairs
 {
-    public class Tests
+    [TestFixture]
+    public class ZipIt
     {
-        [SetUp]
-        public void Setup()
+        [TestCase]
+        public void getLargerNumber()
         {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+            int[] arr1 = new int[] { 13, 64, 5, 7, 88 };
+            int[] arr2 = new int[] { 23, 4, 53, 17, 80 };
+            Assert.AreEqual(Kata.getLargerNumbers(arr1, arr2), new int[] { 23, 64, 53, 17, 88 });
         }
     }
 }
