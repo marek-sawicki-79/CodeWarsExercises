1b6a866c1abda588282b9a347c2e39fff8245135
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-04 21:58:05 +0200

    zip method solution

--- NumberPairs/NumberPairs/Kata.cs
+++ NumberPairs/NumberPairs/Kata.cs
@@ -10,12 +10,13 @@ namespace NumberPairs
     {
         public static int[] getLargerNumbers(int[] a, int[] b)
         {
-            int[] result = new int[a.Length];
-            for (int i = 0; i < a.Length; i++)
-            {
-                _ = (a[i] > b[i]) ? result[i] = a[i] : result[i] = b[i];
-            }
-            return result;
+            //int[] result = new int[a.Length];
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    _ = (a[i] > b[i]) ? result[i] = a[i] : result[i] = b[i];
+            //}
+            //return result;
+            return a.Zip(b, (x, y) => Math.Max(x, y)).ToArray();
         }
     }
 }
