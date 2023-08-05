c3209b782f79fb1069d6d555d500302dc6af2494
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-05 20:52:20 +0200

    Exercise prep

---
+++ PronicNumber/PronicNumber/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PronicNumber
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
