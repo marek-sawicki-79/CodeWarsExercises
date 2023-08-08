b7c4b5113ff78b4f606fcdd2cb56adbcc99ee03b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-08 11:18:29 +0200

    exercise prep

---
+++ ElapsedSeconds/ElapsedSeconds/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ElapsedSeconds
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
