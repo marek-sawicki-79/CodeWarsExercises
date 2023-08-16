6a4f59c2aa25ccb9ac2df35d918daee3790d908d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-16 21:33:32 +0200

    minutes are f-up, will take DateTime

--- EasyTimeConvert/EasyTimeConvert/Class1.cs
+++ EasyTimeConvert/EasyTimeConvert/Class1.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EasyTimeConvert
 {
-    internal class Class1
+    public static class Kata
     {
+        public static string TimeConvert(int num)
+        {
+            int hours = 0;
+            int minutes = 0;
+            string result = "";
+            if (num <= 0) return "00:00";
+            hours = num / 60;
+            minutes = num % 60;
+            result = (hours < 10) ? $"0{hours}:{minutes}"
+                : (minutes < 10) ? $"{hours}:0{minutes}" : $"{hours}:{minutes}";
+            return result;
+        }
     }
 }
--- EasyTimeConvert/EasyTimeConvert/UnitTest1.cs
+++ EasyTimeConvert/EasyTimeConvert/UnitTest1.cs
@@ -1,16 +1,27 @@
 namespace EasyTimeConvert
 {
-    public class Tests
+    [TestFixture]
+    public class SampleTest
     {
-        [SetUp]
-        public void Setup()
+        private static IEnumerable<TestCaseData> testCases
         {
+            get
+            {
+                yield return new TestCaseData(0).Returns("00:00");
+                yield return new TestCaseData(-6).Returns("00:00");
+                yield return new TestCaseData(8).Returns("00:08");
+                yield return new TestCaseData(32).Returns("00:32");
+                yield return new TestCaseData(75).Returns("01:15");
+                yield return new TestCaseData(63).Returns("01:03");
+                yield return new TestCaseData(134).Returns("02:14");
+                yield return new TestCaseData(180).Returns("03:00");
+                yield return new TestCaseData(970).Returns("16:10");
+                yield return new TestCaseData(565757).Returns("9429:17");
+            }
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        [Test, TestCaseSource("testCases")]
+        public string Test(int num) =>
+          Kata.TimeConvert(num);
     }
 }
