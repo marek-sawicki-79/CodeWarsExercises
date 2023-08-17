f3f22d1605d6e68dc05473eb0f1532686e0c8e86
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-17 11:28:07 +0200

    Exercise prep

---
+++ GeneticAlgorithmGenerate/GeneticAlgorithmGenerate/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GeneticAlgorithmGenerate
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
