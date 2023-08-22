38f8f157a419519f90c20b23fa428a2565518018
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-22 23:09:03 +0200

    exercise prep

---
+++ TheRejectFunction/TheRejectFunction/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TheRejectFunction
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
