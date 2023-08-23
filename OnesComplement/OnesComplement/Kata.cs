f8e9f5277a9e86fbe0df81e6925e858b01751081
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-23 23:26:15 +0200

    oneliner with linq from cw

--- OnesComplement/OnesComplement/Kata.cs
+++ OnesComplement/OnesComplement/Kata.cs
@@ -10,10 +10,12 @@ namespace OnesComplement
     {
         public static string OnesComplement(string n)
         {
-            string result = n.Replace('0', '2');
-            result = result.Replace('1', '0');
-            result = result.Replace('2', '1');
-            return result;
+            //string result = n.Replace('0', '2');
+            //result = result.Replace('1', '0');
+            //result = result.Replace('2', '1');
+            //return result;
+
+            return string.Concat(n.Select(x => x == '1' ? '0' : '1'));
         }
     }
 }
