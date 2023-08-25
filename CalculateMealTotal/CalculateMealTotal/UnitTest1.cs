1bf4cf895f9d6b29e33c3a98321cb4201504b62d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-25 22:57:08 +0200

    exercise prep

---
+++ CalculateMealTotal/CalculateMealTotal/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CalculateMealTotal
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
