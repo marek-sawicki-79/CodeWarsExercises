bb8f278118f66e057a630a570898fdabf7e8880e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-27 22:23:24 +0200

    done

--- SumOfDiffrencesBetweenProductsAndLcm/SumOfDiffrencesBetweenProductsAndLcm/Kata.cs
+++ SumOfDiffrencesBetweenProductsAndLcm/SumOfDiffrencesBetweenProductsAndLcm/Kata.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace SumOfDiffrencesBetweenProductsAndLcm
 {
-    internal class Kata
+    public class Kata
     {
+        public static int SumDifferencesBetweenProductsAndLCMs(int[][] pairs)
+        {
+            int[] results = new int[pairs.Length];
+            
+            for(int i = 0; i < pairs.Length; i++)
+            {
+                int[] pair = pairs[i];
+                int a = pair[0];
+                int b = pair[1];
+                int product = a * b;
+                
+                int gcd = GCD(a, b);
+                int lcm = (b != 0) ? (a / gcd) * b : 0;
+                
+                results[i] = Math.Abs(product - lcm);
+            }
+            return results.Sum();
+        }
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
     }
 }
+
--- SumOfDiffrencesBetweenProductsAndLcm/SumOfDiffrencesBetweenProductsAndLcm/UnitTest1.cs
+++ SumOfDiffrencesBetweenProductsAndLcm/SumOfDiffrencesBetweenProductsAndLcm/UnitTest1.cs
@@ -1,16 +1,16 @@
 namespace SumOfDiffrencesBetweenProductsAndLcm
 {
-    public class Tests
+    [TestFixture]
+    public class KataTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void BasicTests()
         {
-            Assert.Pass();
+            Assert.AreEqual(840, Kata.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 15, 18 }, new int[] { 4, 5 }, new int[] { 12, 60 } }));
+            Assert.AreEqual(1092, Kata.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 1, 1 }, new int[] { 0, 0 }, new int[] { 13, 91 } }));
+            Assert.AreEqual(0, Kata.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 15, 7 }, new int[] { 4, 5 }, new int[] { 19, 60 } }));
+            Assert.AreEqual(1890, Kata.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 20, 50 }, new int[] { 10, 10 }, new int[] { 50, 20 } }));
+            Assert.AreEqual(0, Kata.SumDifferencesBetweenProductsAndLCMs(new int[][] { }));
         }
     }
 }
