69c135c2a589d3897d631dbb432dce39b30b4c0d
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-30 20:20:03 +0200

    exercise prep

---
+++ EverythingIsEven/EverythingIsEven/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EverythingIsEven
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
