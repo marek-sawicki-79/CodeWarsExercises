58ab1a7a4f380b714875e8c662f4dc539184662b
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-08-31 11:15:18 +0200

    exercise prep

---
+++ SortByHeight/SortByHeight/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SortByHeight
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
