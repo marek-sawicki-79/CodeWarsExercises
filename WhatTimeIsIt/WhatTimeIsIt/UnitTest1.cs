5af9b4a496371aa2193c8ccdd7dc94208f3bfe13
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-09-01 13:17:07 +0200

    exercise prep

---
+++ WhatTimeIsIt/WhatTimeIsIt/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WhatTimeIsIt
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
