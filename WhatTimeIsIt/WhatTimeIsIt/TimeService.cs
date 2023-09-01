f35ba6262a4fda93d36230a1095a45e50fa2f60f
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-09-01 13:34:00 +0200

    almost, not covered for 12PM

--- WhatTimeIsIt/WhatTimeIsIt/TimeService.cs
+++ WhatTimeIsIt/WhatTimeIsIt/TimeService.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace WhatTimeIsIt
 {
-    internal class TimeService
+    public class TimeService
     {
+
+        public string GetMilitaryTimeFromStandardTime(string time)
+        {
+            string[] strings = time.Split(':');
+            string[] result = new string[strings.Length];
+            result[1] = strings[1];
+            result[2] = strings[2].Substring(0, strings[2].Length - 2);
+
+            if (strings[2].EndsWith("PM"))
+            {
+                result[0] = (int.Parse(strings[0]) + 12).ToString();
+            }
+            else if (strings[0] == "12" && strings[2].EndsWith("AM"))
+            {
+                result[0] = "00";
+            }
+            else
+            {
+                result[0] = strings[0];
+            }
+            return string.Join(":", result);
+        }
     }
 }
--- WhatTimeIsIt/WhatTimeIsIt/UnitTest1.cs
+++ WhatTimeIsIt/WhatTimeIsIt/UnitTest1.cs
@@ -1,16 +1,17 @@
 namespace WhatTimeIsIt
 {
-    public class Tests
+    [TestFixture]
+    public class TimeServiceTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
-        [Test]
-        public void Test1()
+        private TimeService _timeService = new TimeService();
+
+        [TestCase("12:00:01AM", "00:00:01")]
+        [TestCase("11:46:47PM", "23:46:47")]
+        public void TimeService_can_convert_from_12_hour_time_format_to_24_hour_time_format(string input, string expected)
         {
-            Assert.Pass();
+            var result = _timeService.GetMilitaryTimeFromStandardTime(input);
+            Assert.AreEqual(result, expected);
         }
     }
 }
